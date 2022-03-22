504deaa6f349fd1d03563acbc3060f900cb7dd57
aTsekov <antoni.tsekov@gmail.com>
2022-03-22 20:33:12 +0100

    Email validation with a positive lookbehind

---
+++ 09 RegEx/RegExExercise/06 Extract Emails/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06_Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<=\s|^)[a-z0-9]+[a-z\d\.\-_]+\@[a-z\-]+\.([A-Za-z\-\.]+)+\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection match = Regex.Matches(input, pattern);
+
+            foreach (Match m in match)
+            {
+                Console.WriteLine(m.ToString());
+            }
+        }
+    }
+}
