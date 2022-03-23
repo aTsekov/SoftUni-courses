4fbe0a5877fa5c03c97bf2bd1e46dcd88f685e65
aTsekov <antoni.tsekov@gmail.com>
2022-03-23 19:58:49 +0100

    One Key multiple values test program

---
+++ 06 Associative Arrays/Associative Arrays/OneKeyMultipleValues/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OneKeyMultipleValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<int>> car = new Dictionary<string, List<int>>();
+
+            
+        }
+    }
+}
