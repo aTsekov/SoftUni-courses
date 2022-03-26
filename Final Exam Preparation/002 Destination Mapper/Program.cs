7b13ebf884ffa6bbc732d52b990ae2876894781c
aTsekov <antoni.tsekov@gmail.com>
2022-03-26 11:03:57 +0100

    Regex

---
+++ Final Exam Preparation/002 Destination Mapper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _002_Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+                string input = Console.ReadLine();
+                string pattern = @"(\=|\/)(?<destination>[A-Z][A-Za-z]{2,})(\1)";
+                int travelPoint = 0;
+
+                List<string> destinations = new List<string>();
+
+                MatchCollection matches = Regex.Matches(input, pattern);
+
+                foreach (Match currMatch in matches)
+                {
+                    string currDestination = currMatch.Groups["destination"].Value;
+
+                    destinations.Add(currDestination);
+                    travelPoint += currDestination.Length;
+                }
+
+                Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+                Console.WriteLine($"Travel Points: {travelPoint}");
+
+            
+        }
+    }
+}
+    
