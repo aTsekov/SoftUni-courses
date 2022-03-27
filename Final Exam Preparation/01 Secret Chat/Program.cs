56606cb0620a7c66b691fba75951dca6314f37e7
aTsekov <antoni.tsekov@gmail.com>
2022-03-27 18:15:49 +0200

    String processing

---
+++ Final Exam Preparation/01 Secret Chat/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Secret_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Reveal")
+            {
+                string [] commandTypes = command.Split(new string [] { ":|:" }, StringSplitOptions.RemoveEmptyEntries);
+
+                string commType = commandTypes[0];
+                if (commType == "InsertSpace")
+                {
+                    int index = int.Parse(commandTypes[1]);
+                    message = message.Insert(index, " " );
+
+                    
+                }
+                else if (commType == "Reverse")
+                {
+                    string subStr = commandTypes[1];
+                    if (!message.Contains(subStr))
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+                    else if (message.Contains(subStr))
+                    {
+                        int index = message.IndexOf(subStr);
+                        message = message.Remove(index,subStr.Length);
+                        char [] arr = subStr.ToCharArray();
+                        Array.Reverse(arr);
+                        subStr = new string(arr);
+                        message = message + subStr;
+
+                    }
+
+                    
+                }
+                else if ( commType == "ChangeAll")
+                {
+                    string subStr = commandTypes[1];
+                    string replacement = commandTypes[2];
+
+                    message = message.Replace(subStr, replacement);
+
+                    
+                }
+
+                Console.WriteLine(message);
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
