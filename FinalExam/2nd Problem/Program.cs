630209962f3bc96fcbcdd43f78be3e23eba3cea4
aTsekov <antoni.tsekov@gmail.com>
2022-04-03 10:29:41 +0200

    Regex

---
+++ FinalExam/2nd Problem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2nd_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(@+|#+)(?<color>[a-z]{3,})(@+|#+)\W*(\/+)(?<digit>\d+)(\/+)";
+            string text = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(text, pattern);
+
+            foreach (Match item in matches)
+            {
+                Console.WriteLine($"You found {item.Groups["digit"]} {item.Groups["color"]} eggs!");
+
+            }
+        }
+    }
+}
