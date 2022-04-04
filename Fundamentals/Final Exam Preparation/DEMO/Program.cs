646c94846e59ecbb55a26957b0797bb6e1c86a17
aTsekov <antoni.tsekov@gmail.com>
2022-04-04 19:54:24 +0200

    Restructuring

---
+++ Fundamentals/Final Exam Preparation/DEMO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = a>b ?a:b;
+            Console.WriteLine(c);
+        }
+    }
+}
