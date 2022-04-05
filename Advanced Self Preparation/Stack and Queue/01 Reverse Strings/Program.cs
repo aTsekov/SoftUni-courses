b38ebc4990707f607d0c85522992164cf92e1919
aTsekov <antoni.tsekov@gmail.com>
2022-04-05 20:13:50 +0200

    Reverse a string with a stack

---
+++ Advanced Self Preparation/Stack and Queue/01 Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Stack <char> stack = new Stack<char>();
+
+            foreach (var ch in text)
+            {
+                stack.Push(ch);
+            }
+            foreach (var item in stack)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
