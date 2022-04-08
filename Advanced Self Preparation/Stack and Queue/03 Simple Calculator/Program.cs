f230f234ef30769422ecc4e1cda64d3b1b02de2c
aTsekov <antoni.tsekov@gmail.com>
2022-04-08 21:14:50 +0200

    Stack simple calc.

---
+++ Advanced Self Preparation/Stack and Queue/03 Simple Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<string> stack = new Stack<string>(input .Split(" ",StringSplitOptions.RemoveEmptyEntries).Reverse());
+            while (stack.Count>1)
+            {
+                // 2 + 5 + 10 - 2 - 1
+                int operand1 = int.Parse(stack.Pop()); // искарваме от стака число, знак и число. 
+                string opr = stack.Pop();
+                int operand2 = int.Parse(stack.Pop());
+
+                if (opr == "+")
+                {
+                    stack.Push((operand1 + operand2).ToString());
+                }
+                else if (opr == "-")
+                {
+                    stack.Push((operand1 - operand2).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
