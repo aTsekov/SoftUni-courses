d69ef029f60aa217f3223e9df7f26ae74cb34ade
aTsekov <antoni.tsekov@gmail.com>
2022-04-09 11:23:49 +0200

    Filter even nums in a queue and print them

---
+++ Advanced Self Preparation/Stack and Queue/05 Print Even Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue <int>queue = new Queue<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    queue.Enqueue(arr[i]);
+                    
+                    
+                }
+            }
+            Console.WriteLine(string.Join(", ",queue));
+            
+        }
+    }
+}
