83a714c6a9e621da2b43919e039bc4619e28a813
aTsekov <antoni.tsekov@gmail.com>
2022-04-09 11:40:02 +0200

    Supermarket queue

---
+++ Advanced Self Preparation/Stack and Queue/06 Supermarket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            Queue<string> names = new Queue<string>();
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input != "End")
+                {
+                    
+                    if (input == "Paid")
+                    {
+                        
+                        Console.WriteLine(String.Join("\n",names));
+                        names.Clear();
+                        continue;
+                    }
+                    names.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+
+        }
+    }
+}
