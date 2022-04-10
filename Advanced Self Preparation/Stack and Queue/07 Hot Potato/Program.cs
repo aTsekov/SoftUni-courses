4f7bebfb91bde8aeb7d91557289cb2fa90d05c29
aTsekov <antoni.tsekov@gmail.com>
2022-04-10 10:00:08 +0200

    Commit with Bash

---
+++ Advanced Self Preparation/Stack and Queue/07 Hot Potato/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ').ToArray();
+            int hotPotato= int.Parse(Console.ReadLine());
+            Queue<string> circle = new Queue<string>();
+
+            for (int i = 0; i < names.Length; i++)// пълним си имената в опашката. Можем и директно да четем опашката от конзолата по същия начин като масив,лист, и т.н.
+            {
+
+                circle.Enqueue(names[i]);
+
+            }
+            while (circle.Count >1)//въртим докато не остане само едно дете. 
+            {
+                for (int i = 1; i < hotPotato; i++)
+                {
+                    string kid = circle.Dequeue(); //взимаме първото дете от опашката и го слагаме последно
+                    circle.Enqueue(kid);
+
+
+                }
+
+                Console.WriteLine($"Removed {circle.Dequeue()}");// махаме новото първо дете/слаге го първо на опашката
+            }
+            Console.WriteLine($"Last is{circle.Dequeue()}");
+            
+        }
+    }
+}
