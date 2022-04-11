d6730954f5e34eb9ae6e90e162725cbca837a4ef
aTsekov <antoni.tsekov@gmail.com>
2022-04-11 21:19:15 +0200

    Basic queue operations

--- Advanced Self Preparation/Stack and Queue/E02 Basic Queue Operations/Program.cs
+++ Advanced Self Preparation/Stack and Queue/E02 Basic Queue Operations/Program.cs
@@ -2,44 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace E01_Basic_Stack_Operations
+namespace E02_Basic_Queue_Operations
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int [] arrInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] arrInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int[] arrNums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int nPush = arrInput[0];
             int sPop = arrInput[1];
             int xToSearch = arrInput[2];
 
-            Stack<int> stack = new Stack<int>(arrNums);
-            //Finally an integer X, an element that you should look for in the stack. If it’s found, print "true" on the console. If it isn’t, print the smallest element currently present in the stack. If there are no elements in the sequence, print 0 on the console.
-
+            Queue<int> queue = new Queue<int>(arrNums);
             
+
+
             for (int i = 0; i < sPop; i++)
             {
-                stack.Pop();
+                queue.Dequeue();
             }
-            if (stack.Contains(xToSearch))
+            if (queue.Contains(xToSearch))
             {
                 Console.WriteLine("true");
             }
             else
             {
-                if (stack.Count == 0)
+                if (queue.Count == 0)
                 {
                     Console.WriteLine("0");
                     return;
                 }
-                int minValue = stack.Min();
+                int minValue = queue.Min();
                 Console.WriteLine(minValue);
             }
-            
-
-
-
         }
     }
 }
