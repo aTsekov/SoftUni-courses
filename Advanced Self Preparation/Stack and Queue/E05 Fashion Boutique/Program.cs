c6c5ef5dc5cc2c2a6fc8a275859c68b1b392a37a
aTsekov <antoni.tsekov@gmail.com>
2022-04-12 18:49:32 +0200

    Clothes store

---
+++ Advanced Self Preparation/Stack and Queue/E05 Fashion Boutique/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E05_Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] clothes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(clothes);
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int CONSTRACK = rackCapacity;
+            int racksCounter = 1;
+
+            while (stack.Count != 0)
+            {
+                int currCloth = stack.Peek();
+                if (currCloth <= rackCapacity)
+                {
+                    stack.Pop();
+                    rackCapacity -= currCloth;
+                }
+                else if (currCloth > rackCapacity)
+                {
+                    racksCounter++;
+                    
+                    rackCapacity = CONSTRACK;
+
+                }
+            }
+            Console.WriteLine(racksCounter);
+
+        }
+    }
+}
