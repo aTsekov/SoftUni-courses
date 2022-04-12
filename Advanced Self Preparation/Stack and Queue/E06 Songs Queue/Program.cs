575822cce45f14a44d509a4d46142f17a8dbf2da
aTsekov <antoni.tsekov@gmail.com>
2022-04-12 19:20:49 +0200

    Songs queue

---
+++ Advanced Self Preparation/Stack and Queue/E06 Songs Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E06_Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] inputSongs = Console.ReadLine().Split(", ");
+            Queue <string> queue = new Queue<string>(inputSongs); 
+            while (queue.Count != 0)
+            {
+
+                string commandType = Console.ReadLine();
+                
+                if (commandType == "Play")
+                {
+                    queue.Dequeue();
+                }
+                else if (commandType.StartsWith("Add"))
+                {
+                    string song = commandType.Substring(4);
+                    if (queue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(song);
+                    }
+                    
+                    
+                }
+                else if (commandType == "Show")
+                {
+                    Console.WriteLine(string.Join(", ",queue));
+                }
+
+            }
+            Console.WriteLine($"No more songs!");
+        }
+    }
+}
