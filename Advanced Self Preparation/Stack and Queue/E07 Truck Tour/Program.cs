23afe8314f5c8da695f5221ed6807acd2a9b721c
aTsekov <antoni.tsekov@gmail.com>
2022-04-13 19:39:56 +0200

    Truck Tour

---
+++ Advanced Self Preparation/Stack and Queue/E07 Truck Tour/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E07_Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPums = int.Parse(Console.ReadLine());
+
+            Queue<int[]> queue = new Queue<int[]>();
+
+            for (int i = 0; i < numberOfPums; i++)
+            {
+                int[] PumpInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                queue.Enqueue(PumpInfo);
+            }
+            int startIndex = 0;
+            while (true)
+            {
+                bool isValidPump = true;
+
+                int truckFuelAmount = 0;
+                foreach (int[] item in queue)
+                {
+
+                    int litters = item[0];
+                    truckFuelAmount += litters;
+                    int distance = item[1];
+                    if (truckFuelAmount - distance < 0)
+                    {
+                        startIndex += 1;
+                        int[] currentPum = queue.Dequeue();
+                        queue.Enqueue(currentPum);
+                        isValidPump = false;
+                        break;
+                    }
+                    truckFuelAmount -= distance;
+                }
+                if (isValidPump)
+                {
+                    Console.WriteLine(startIndex);
+                    break;
+                }
+            }
+        }
+    }
+}
