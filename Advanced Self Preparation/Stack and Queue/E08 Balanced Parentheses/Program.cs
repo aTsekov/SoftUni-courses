413a96a3cceb08d5ebbfd3509d3100c8d848b487
aTsekov <antoni.tsekov@gmail.com>
2022-04-13 20:17:40 +0200

    66/100 to be fixed at a later stage

---
+++ Advanced Self Preparation/Stack and Queue/E08 Balanced Parentheses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E08_Balanced_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string expression = Console.ReadLine();
+
+            Stack<char> openParentheses = new Stack<char>();
+            foreach (char ch in expression)
+            {
+                if (ch =='('|| ch == '[' || ch == '{')
+                {
+                    openParentheses.Push(ch);
+                }
+                else
+                {
+                    char currentOpenParentheses
+                }
+            }
+        }
+    }
+}
