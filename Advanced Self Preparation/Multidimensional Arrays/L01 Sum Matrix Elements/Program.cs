ba33bd80b8d4abb71fd8ebe1734555cbec75bb70
aTsekov <antoni.tsekov@gmail.com>
2022-04-16 23:05:00 +0200

    Multidimensional Arrays

---
+++ Advanced Self Preparation/Multidimensional Arrays/L01 Sum Matrix Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace L01_Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(", ")
+                .Select(int.Parse).ToArray();
+
+            //Първо Ред, после колони
+            int[,] matrix = new int[size[0],size[1]];
+
+            for (int row =  0; row < matrix.GetLength(0); row++)
+            {
+                int [] currentRow = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row,col] = currentRow[col];
+                }
+
+            }
+            Console.WriteLine(size[0]);
+            Console.WriteLine(size[1]);
+
+            int sum = 0;
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
