cab184d7abd89ef0267ed62b9b0b2af0ffe87f26
aTsekov <antoni.tsekov@gmail.com>
2022-04-18 18:17:26 +0200

    Sum Columns

---
+++ Advanced Self Preparation/Multidimensional Arrays/L02 Sum Matrix Columns/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace L02_Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(", ")
+                .Select(int.Parse).ToArray();
+
+            //Първо Ред, после колони
+            int[,] matrix = new int[size[0], size[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currentRow = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+
+            }
+        }
+    }
+}
