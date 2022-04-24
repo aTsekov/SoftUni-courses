e44df41dd880d1c7d71aba98821468d953554591
aTsekov <antoni.tsekov@gmail.com>
2022-04-24 10:16:40 +0200

    Difference in sum between the first and the second diagonal

---
+++ Advanced Self Preparation/Multidimensional Arrays/E01 Diagonal Difference/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace E01_Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int sumFirstDiag = 0;
+            int sumSecondDiag = 0;
+            int counterSecondDiag = matrix.GetLength(0) -1;
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+
+
+                }
+                int currentDigitBackwards = matrix[row,counterSecondDiag]; 
+                counterSecondDiag--;
+                sumSecondDiag += currentDigitBackwards;
+                int currDigit = matrix[row, row];
+                sumFirstDiag += currDigit;
+            }
+            int difference = Math.Abs(sumFirstDiag - sumSecondDiag);
+
+
+
+
+            Console.WriteLine(sumFirstDiag);
+            Console.WriteLine(sumSecondDiag);
+            Console.WriteLine(difference);
+        }
+    }
+}
