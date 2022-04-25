ad7c01b0728ab5fd8a956bce25490850fc826059
aTsekov <antoni.tsekov@gmail.com>
2022-04-25 20:06:37 +0200

    Empty task for the moment

---
+++ Advanced Self Preparation/Multidimensional Arrays/E05 Snake Moves/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E05_Snake_Moves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
