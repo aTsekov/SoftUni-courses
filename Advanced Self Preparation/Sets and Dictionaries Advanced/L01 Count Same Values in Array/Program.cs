9349e0a1fddd9973bb606d18052ce4500a3c5675
aTsekov <antoni.tsekov@gmail.com>
2022-04-26 21:56:48 +0200

    Summary of Dictionaries

---
+++ Advanced Self Preparation/Sets and Dictionaries Advanced/L01 Count Same Values in Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L01_Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             double [] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            Dictionary <double, int> counter = new Dictionary<double, int>();
+
+            foreach (var number in input)
+            {
+                if (counter.ContainsKey(number))
+                {
+                    counter[number] += 1;
+                }
+                else
+                {
+                    counter[number] = 1;
+                }
+            }
+
+            foreach (var item in counter)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
