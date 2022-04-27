325fac5e39db3ed99eb20761c1d2c4dd41cbb072
aTsekov <antoni.tsekov@gmail.com>
2022-04-27 18:57:19 +0200

    Solved with SortedHash<string>

---
+++ Advanced Self Preparation/Sets and Dictionaries Advanced/E03 Periodic Table/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E03_Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+                for (int k = 0; k < input.Length; k++)
+                {
+                    periodicTable.Add(input[k]);
+                }
+
+            }
+
+            foreach (var item in periodicTable)
+            {
+                Console.Write($"{string.Join(' ',item)} ");
+            }
+        }
+    }
+}
