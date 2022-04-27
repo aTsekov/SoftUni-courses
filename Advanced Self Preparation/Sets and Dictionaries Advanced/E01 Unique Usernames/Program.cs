ee74cf8777e45e46e6dc0ad8bb442c53072e8176
aTsekov <antoni.tsekov@gmail.com>
2022-04-27 18:21:34 +0200

    Solved with Hashset

---
+++ Advanced Self Preparation/Sets and Dictionaries Advanced/E01 Unique Usernames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E01_Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string inputUserName = Console.ReadLine();
+
+                usernames.Add(inputUserName);
+            }
+
+            foreach (var item in usernames)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
