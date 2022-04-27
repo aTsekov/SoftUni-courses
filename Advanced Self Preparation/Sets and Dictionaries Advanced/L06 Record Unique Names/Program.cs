288b6dfaeb4a793884b3e4f51c31ccc4c737e2ae
aTsekov <antoni.tsekov@gmail.com>
2022-04-27 15:15:39 +0200

    Unsorted HashSet

---
+++ Advanced Self Preparation/Sets and Dictionaries Advanced/L06 Record Unique Names/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L06_Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
