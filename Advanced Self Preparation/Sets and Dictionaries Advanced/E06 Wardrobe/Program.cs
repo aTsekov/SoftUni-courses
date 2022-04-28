d022534941c3599a22fd7c33e053ae5009a0260c
aTsekov <antoni.tsekov@gmail.com>
2022-04-28 21:15:36 +0200

    Nested Dictionaries.

---
+++ Advanced Self Preparation/Sets and Dictionaries Advanced/E06 Wardrobe/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numClothes = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> colorsWardrobe
+                = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < numClothes; i++)
+            {
+                string[] input = Console.ReadLine()
+                      .Split(" -> ");
+                string color = input[0];
+                string[] clothes = input[1].Split(",");
+
+                if (!colorsWardrobe.ContainsKey(color))
+                {
+                    colorsWardrobe[color] = new Dictionary<string, int>();
+                }
+
+                var colorCollection = colorsWardrobe[color];
+
+                for (int j = 0; j < clothes.Length; j++)
+                {
+                    if (!colorCollection.ContainsKey(clothes[j]))
+                    {
+                        colorCollection[clothes[j]] = 0;
+                    }
+                    colorCollection[clothes[j]]++;
+                }
+            }
+
+            string[] itemNeeded = Console.ReadLine().Split();
+
+            string colorNeeded = itemNeeded[0];
+            string pieceNeeded = itemNeeded[1];
+
+            foreach (var color in colorsWardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                foreach (var clothes in color.Value)
+                {
+                    Console.WriteLine(color.Key == colorNeeded && clothes.Key == pieceNeeded
+                        ? $"* {clothes.Key} - {clothes.Value} (found!)"
+                        : $"* {clothes.Key} - {clothes.Value}");
+                }
+            }
+        }
+    }
+}
