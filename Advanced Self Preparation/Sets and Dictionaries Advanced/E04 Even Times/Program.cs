4d4594b4051e41b57b16caa9eaa1a334a63da9c4
aTsekov <antoni.tsekov@gmail.com>
2022-04-28 15:08:31 +0200

    80/100 seek for mistake

---
+++ Advanced Self Preparation/Sets and Dictionaries Advanced/E04 Even Times/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E04_Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int counter = 1;
+            int numToSave = 0;
+
+            HashSet<int> nums = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int m = int.Parse(Console.ReadLine());
+                
+
+                if (nums.Contains(m))
+                {
+                    counter++;
+                    numToSave = m;
+                }
+                nums.Add(m);
+            }
+            Console.WriteLine(numToSave);
+
+
+        }
+    }
+}
