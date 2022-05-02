c693d74c4c74980535a2323d6550a0d5e8b1d8a2
aTsekov <antoni.tsekov@gmail.com>
2022-05-02 18:00:01 +0200

    Stream Reader and Stream Writer

---
+++ Advanced Self Preparation/Streams, Files and Directories/L01 Odd Line/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace L01_Odd_Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputPath = Path.Combine("Data","input.txt");
+            var outputPath = Path.Combine("Data", "output.txt");
+
+            using (StreamReader input = new StreamReader(inputPath))
+            {
+                using (StreamWriter output = new StreamWriter(outputPath))
+                {
+                    string line  = input.ReadLine();
+                    int count = 0;
+
+                    while (line != null)
+                    {
+                        if (count % 2 != 0)
+                        {
+                            output.WriteLine(line);
+                        }
+
+                        count++;
+                        line = input.ReadLine();
+                    }
+
+                }
+                
+
+            }
+        }
+    }
+}
