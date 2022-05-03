37c78803647513a67e584e77f05939ce5efcc490
aTsekov <antoni.tsekov@gmail.com>
2022-05-03 18:08:01 +0200

    writing output into bytes

---
+++ Advanced Self Preparation/Streams, Files and Directories/L05 Extract Special Bytes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace L05_Extract_Special_Bytes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string exampleImage = Path.Combine("exampleimage.png");
+            string bytes = Path.Combine("bytes.txt");
+            string output = Path.Combine("output.bin");
+
+            using (StreamReader image = new StreamReader(exampleImage))
+            {
+                string imageOutput = image.ReadToEnd();
+
+                using (StreamReader textoutput = new StreamReader(bytes))
+                {
+                    string text = textoutput.ReadToEnd();
+
+
+                    using (StreamWriter writeOutputInBytes = new StreamWriter(output))
+                    {
+                        writeOutputInBytes.WriteLine(imageOutput);
+                        writeOutputInBytes.WriteLine(text);
+
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
+}
