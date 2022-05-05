cc3366857c41a04e0cdc87e13d12f16256329587
aTsekov <antoni.tsekov@gmail.com>
2022-05-05 18:11:32 +0200

    Changed to print only the even lines

--- Advanced Self Preparation/Streams, Files and Directories/E01 Even Lines/Program.cs
+++ Advanced Self Preparation/Streams, Files and Directories/E01 Even Lines/Program.cs
@@ -9,6 +9,7 @@ namespace E01_Even_Lines
         static void Main(string[] args)
         {
             string inputFilePath = Path.Combine("text.txt");
+            int counter = 0;
 
             using (StreamReader sr = new StreamReader(inputFilePath)) //We create a new stream reader that reads the file content
             {
@@ -16,14 +17,22 @@ namespace E01_Even_Lines
                 string line = sr.ReadLine(); //we read a line from the text file
                 while (line != null) // we itterate, until there are no lines left to read.
                 {
+                   
+                    
+
                     foreach (var symbol in specialSymbols) // each symbol we replace with @. line is = line because string is imputable and we have to rewrite its value. 
                     {
                         line = line.Replace(symbol, "@");
 
                     }
 
-                     
-                    Console.WriteLine(string.Join(" ", line.Split().Reverse())); // we threat line an array now >> we reverce it and join it by " " so it can be printed. 
+                    if (counter % 2 == 0)
+                    {
+                        Console.WriteLine(string.Join(" ", line.Split().Reverse())); // we threat line an array now >> we reverce it and join it by " " so it can be printed. 
+                    }
+                    counter++;
+                    
+                    
                     line = sr.ReadLine(); // we read the next line and repeat until we have lines. 
                 }
 
