5b9933b39895a654269670ce2897f112725db836
aTsekov <antoni.tsekov@gmail.com>
2022-05-05 12:26:55 +0200

    Byte reader and Byte writer

--- Advanced Self Preparation/Streams, Files and Directories/L05 Extract Special Bytes/Program.cs
+++ Advanced Self Preparation/Streams, Files and Directories/L05 Extract Special Bytes/Program.cs
@@ -8,14 +8,19 @@ namespace L05_Extract_Special_Bytes
         static void Main(string[] args)
         {
 
+            byte[] data = new byte[] {64, 83,111,102,116,33 };// we put the bytes we want to write in the file
+
+
+
+            using  FileStream fileWriter =  new FileStream("Testoutput.txt",FileMode.OpenOrCreate);  
             using  FileStream fileReader =  new FileStream("output.txt",FileMode.OpenOrCreate);
             byte[] buffer = new byte[100];
             fileReader.Read(buffer, 0, 10); // Write in the buffer, start from the 0 (from the very begining) and put 10 bytes in the buffer
 
             //We aread as bytes the text that we have in the "output" file. and we are doing it with a buffer of 10 bytes. 
 
-            
 
+            fileWriter.Write(data, 0, data.Length); // in the file they appear as normal text. 
 
         }
     }
