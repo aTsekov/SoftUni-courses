aa0a6a200f8f833d7a6bcdb1e758c004d648c6c2
aTsekov <antoni.tsekov@gmail.com>
2022-05-05 17:48:22 +0200

    We exchange some special symbols and replace them with @ while we read line by line from a text file. StreamReader.

---
+++ Advanced Self Preparation/Streams, Files and Directories/E01 Even Lines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace E01_Even_Lines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = Path.Combine("text.txt");
+
+            using (StreamReader sr = new StreamReader(inputFilePath)) //We create a new stream reader that reads the file content
+            {
+                string[] specialSymbols = new [] { "-", ",", ".", "!", "?" }; // we make an array with the special symbols that we will replace later on
+                string line = sr.ReadLine(); //we read a line from the text file
+                while (line != null) // we itterate, until there are no lines left to read.
+                {
+                    foreach (var symbol in specialSymbols) // each symbol we replace with @. line is = line because string is imputable and we have to rewrite its value. 
+                    {
+                        line = line.Replace(symbol, "@");
+
+                    }
+
+                     
+                    Console.WriteLine(string.Join(" ", line.Split().Reverse())); // we threat line an array now >> we reverce it and join it by " " so it can be printed. 
+                    line = sr.ReadLine(); // we read the next line and repeat until we have lines. 
+                }
+
+            }
+        }
+    }
+}
--- Advanced Self Preparation/Streams, Files and Directories/L07 Folder Size/Program.cs
+++ Advanced Self Preparation/Streams, Files and Directories/L07 Folder Size/Program.cs
@@ -8,25 +8,26 @@ namespace L07_Folder_Size
     {
         static void Main(string[] args)
         {
-
+            // We get the directory path of the folder that we want to see the size in bytes.
             string directoryPath = @"C:\Users\Antoni\Desktop\SoftUni\SoftUni-courses\Advanced Self Preparation\Streams, Files and Directories";
 
-            var totalLenght =  GetTotalLenghts(directoryPath);
+            var totalLenght =  GetTotalLenghts(directoryPath); // we made a recursive method (method that cals itselc) s owe can not only calculate the files in the main folder, but also its sub-directories. 
 
             Console.WriteLine(totalLenght);
         }
         static long GetTotalLenghts (string directoryPath)
         {
-            string[] files = Directory.GetFiles(directoryPath);
+            string[] files = Directory.GetFiles(directoryPath);// We make an array that contains each file name
             long totalLenght = 0;
 
             foreach (string file in files)
             {
-                totalLenght += new FileInfo(file).Length;
+                totalLenght += new FileInfo(file).Length; //we itterate trhough each file in the array and add it to totalLenght to calc all files size and add it to the variable (and the problem is solved to here)
 
             }
 
-            var subDirectories = Directory.GetDirectories(directoryPath);
+            // However if we want to include the sub directories we have to call the method (recursive) in itself 
+            string [] subDirectories = Directory.GetDirectories(directoryPath);// we get each of the sub direactories in an array and we itterate them and add them to the totalLenght with the method itself. 
             foreach (var directory in subDirectories)
             {
                 totalLenght += GetTotalLenghts(directory);
