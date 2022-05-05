d315478403407db5d534c94d4aafad8cb4c1489e
aTsekov <antoni.tsekov@gmail.com>
2022-05-05 18:47:51 +0200

    Copy binary files

---
+++ Advanced Self Preparation/Streams, Files and Directories/E03 Copy Binary File/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace E03_Copy_Binary_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //File.Copy("CopyMe.png", "CopyMeCopy.png");
+            FileStream fileReader = new FileStream("CopyMe.png", FileMode.Open);
+
+            FileStream fileWriter = new FileStream("CopyMeCopy.png", FileMode.Create);
+            byte[] arrayOfBytes = new byte[256];
+            while (true)
+            {
+                int currentBytes = fileReader.Read(arrayOfBytes, 0, arrayOfBytes.Length);
+
+                if (currentBytes == 0)
+                {
+                    break;
+                }
+                fileWriter.Write(arrayOfBytes, 0, arrayOfBytes.Length);
+
+            }
+            Console.WriteLine("Done");
+            fileWriter.Close();
+
+        }
+    }
+}
