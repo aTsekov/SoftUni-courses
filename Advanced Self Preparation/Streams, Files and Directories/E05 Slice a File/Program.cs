22eecc65125111535c2b17d5707e12c74bc034b8
aTsekov <antoni.tsekov@gmail.com>
2022-05-05 14:15:40 +0200

    Slice a big file to 4 equal parts

---
+++ Advanced Self Preparation/Streams, Files and Directories/E05 Slice a File/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace E05_Slice_a_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fileStream = new FileStream("countries.txt", FileMode.OpenOrCreate);// we create a stream that reads the big file
+
+            byte[] data = new byte [fileStream.Length];// we create an array with the lenght if the big file.
+            fileStream.Read(data); // we read the array
+
+            int bytesPerFile =(int) Math.Ceiling( fileStream.Length/ 4.0); // We create int variable that is = to the bih file divided by 4 (we want to have 4 files) and we add the Math.Ceiling to round upper so we can have 3 files with a few byts more and the last file would containt a bit less byts but this way we will make sure we do not miss a byte if the divide to 4 will not leave a residue
+
+            for (int i = 0; i < 4; i++)
+            {
+                byte [] buffer = new byte[bytesPerFile]; // we make a buffer  that is = to the bytes pe file
+                fileStream.Read (buffer); // we read the file with the size of the buffer. 
+
+                using FileStream writer = new FileStream($"Part - {i}.txt", FileMode.OpenOrCreate); // we create a new file each new itteration with the size of the buffer. 
+                writer.Write (buffer);
+            }
+
+
+        }
+    }
+}
