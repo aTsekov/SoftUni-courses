36da54cb7abcba8665a4325c74c97bca29dc5902
aTsekov <antoni.tsekov@gmail.com>
2022-05-05 17:07:38 +0200

    Get the size of all files + recursive method that will include all od the sub-folders

---
+++ Advanced Self Preparation/Streams, Files and Directories/L07 Folder Size/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.IO;    
+
+namespace L07_Folder_Size
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string directoryPath = @"C:\Users\Antoni\Desktop\SoftUni\SoftUni-courses\Advanced Self Preparation\Streams, Files and Directories";
+
+            var totalLenght =  GetTotalLenghts(directoryPath);
+
+            Console.WriteLine(totalLenght);
+        }
+        static long GetTotalLenghts (string directoryPath)
+        {
+            string[] files = Directory.GetFiles(directoryPath);
+            long totalLenght = 0;
+
+            foreach (string file in files)
+            {
+                totalLenght += new FileInfo(file).Length;
+
+            }
+
+            var subDirectories = Directory.GetDirectories(directoryPath);
+            foreach (var directory in subDirectories)
+            {
+                totalLenght += GetTotalLenghts(directory);
+            }
+
+            return totalLenght;
+        }
+    }
+}
