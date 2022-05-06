daa6065fb4960c0c1b9743443b6a020dab7ed555
aTsekov <antoni.tsekov@gmail.com>
2022-05-06 17:19:40 +0200

    LINQ Select, Where, OrderBy

---
+++ Advanced Self Preparation/Functional Programming/L01 Sort Even Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace L01_Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var newList = line.Split(", ").Select(int.Parse).Where( x => x % 2 == 0).OrderBy(x => x);
+
+            Console.WriteLine(String.Join(", ", newList));
+
+        }
+    }
+}
