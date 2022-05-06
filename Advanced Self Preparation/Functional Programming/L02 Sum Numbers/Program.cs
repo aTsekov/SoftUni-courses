4d16992175d5a8c494da4446644fc45b815c43b8
aTsekov <antoni.tsekov@gmail.com>
2022-05-06 17:53:34 +0200

    Find all words starting with a capital letter and print them

---
+++ Advanced Self Preparation/Functional Programming/L02 Sum Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace L02_Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(MyParse);
+
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+        }
+        static int MyParse (string numberAsString)
+        {
+            int number = 0;
+            foreach (var digit  in numberAsString)
+            {
+                number *= 10;
+                number += (digit - '0');
+            }
+            return number;
+        }
+    }
+}
---
+++ Advanced Self Preparation/Functional Programming/L03 Count Uppercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace L03_Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var upperLetterWords = words.Where(word => char.IsUpper(word[0]));
+            foreach (var word  in upperLetterWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
