e921b0f815d5b6dbc3f673689f67b3257cda0e59
aTsekov <antoni.tsekov@gmail.com>
2022-05-07 09:24:57 +0200

    Solved with two Selects in a row

---
+++ Advanced Self Preparation/Functional Programming/L04 Add VAT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L04_Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
