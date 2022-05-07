d3e8a94fd978e7b6ba7753cb256a73d30b7df94c
aTsekov <antoni.tsekov@gmail.com>
2022-05-07 09:56:29 +0200

    Solved with dynamic filters

--- Advanced Self Preparation/Functional Programming/L04 Add VAT/Program.cs
+++ Advanced Self Preparation/Functional Programming/L04 Add VAT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace L04_Add_VAT
 {
@@ -6,7 +7,12 @@ namespace L04_Add_VAT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] numbWithVat =Console.ReadLine().Split(", ").Select(double.Parse).Select(x => x* 1.2).ToArray();
+
+            foreach (var num  in numbWithVat)
+            {
+                Console.WriteLine($"{num:F2}");
+            }
         }
     }
 }
---
+++ Advanced Self Preparation/Functional Programming/L05 Filter By Age/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L05_Filter_By_Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
