e9eb3ea8bf75fccecea35bd7282e356144c2a26d
aTsekov <antoni.tsekov@gmail.com>
2022-05-08 11:14:33 +0200

    Func < int[], int> getMinNum

---
+++ Advanced Self Preparation/Functional Programming/E03 Custom Min Function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace E03_Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Func < int[], int> getMinNum = input =>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var number in input)
+                {
+                    if (number < minValue)
+                    {
+                        minValue = number;
+                    }
+                }
+                return minValue;
+            };
+
+            Console.WriteLine(getMinNum(input));
+        }
+    }
+}
