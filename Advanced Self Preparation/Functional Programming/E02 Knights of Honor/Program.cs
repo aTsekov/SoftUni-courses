e5df9e7b774eb33d6e8148af2d2600bfcb5e1832
aTsekov <antoni.tsekov@gmail.com>
2022-05-08 10:51:48 +0200

    Action<List<string>> printCollection

---
+++ Advanced Self Preparation/Functional Programming/E02 Knights of Honor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E02_Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> names = Console.ReadLine().Split(" ").ToList(); // Read the input names and split them by empty space
+
+            Action<List<string>> printCollection = name => name.ForEach(name => Console.WriteLine($"Sir {name}"));
+
+            printCollection(names);
+
+
+            //This is less convinient way of doing the same. 
+            //string[] names = Console.ReadLine().Split(" "); // Read the input names and split them by empty space
+
+            //Action<string[]> printCollection = name => Console.WriteLine("Sir " + string.Join("\nSir ", name));
+
+            //printCollection(names);
+        }
+    }
+}
