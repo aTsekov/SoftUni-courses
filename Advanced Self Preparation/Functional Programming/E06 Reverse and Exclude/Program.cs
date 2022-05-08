390e64dfc711a9ed2ef8e404d68953a693fe8157
aTsekov <antoni.tsekov@gmail.com>
2022-05-08 21:51:17 +0200

    Solved with Action

---
+++ Advanced Self Preparation/Functional Programming/E06 Reverse and Exclude/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E06_Reverse_and_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            
+            int n = int.Parse(Console.ReadLine());
+
+            numbers.Reverse();
+
+            Action<List<int>> removeDivisible = nums =>
+            {
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (nums[i] % n != 0)
+                    {
+                        Console.Write($"{nums[i]} ");
+                    }
+                }
+
+
+            };
+
+            removeDivisible(numbers);
+        }
+    }
+}
