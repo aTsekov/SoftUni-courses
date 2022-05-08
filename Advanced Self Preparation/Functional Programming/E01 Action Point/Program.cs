6a73dd1547c06f4db184dba11dd94e8bb0838445
aTsekov <antoni.tsekov@gmail.com>
2022-05-08 10:34:29 +0200

    Print with Action <string[]>

---
+++ Advanced Self Preparation/Functional Programming/E01 Action Point/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace E01_Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string [] names = Console.ReadLine().Split(" "); // Read the input names and split them by empty space
+
+            Action<string []> printCollection = name => Console.WriteLine(string.Join("\n",name)); // create an action of type string [] array. printCollection is the name of the functions. name is tha lambda func. 
+
+            printCollection(names);
+            
+
+
+
+
+        }
+        
+    }
+}
