0b43458ad95623134ecc30b48470f565b3bb59aa
aTsekov <antoni.tsekov@gmail.com>
2022-05-09 20:05:29 +0200

    Func<string, int, bool>

---
+++ Advanced Self Preparation/Functional Programming/E07 Predicate for Names/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E07_Predicate_for_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
