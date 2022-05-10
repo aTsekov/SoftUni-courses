70e55aeeff2823625ae1a9ce3a49b3558231c249
aTsekov <antoni.tsekov@gmail.com>
2022-05-10 22:56:06 +0200

    Class with normal public properties

---
+++ Advanced Self Preparation/Defining Classes/L01 Car/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CarManufacturer;
+using System.Linq;
+
+namespace CarManufacturer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Model = "MK3";
+            car.Make = "VW";
+            car.Year = 1992;
+
+
+
+            Console.WriteLine($"Make: {car.Model}\nModel: {car}\nYear: {car}");
+
+        }
+    }
+    public class Car
+    {
+
+        public Car()
+        {
+
+        }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+
+
+
+    }
+    public class StartUp
+    {
+
+    }
+}
