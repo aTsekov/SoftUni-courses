20256d350ec6e95263a3ae8dcce847edd49b544e
aTsekov <antoni.tsekov@gmail.com>
2022-05-12 17:55:27 +0200

    Classes multiple files

---
+++ Advanced Self Preparation/Defining Classes/Classes/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name; // field. It can be used only in the class because it is private
+        private int age;
+
+        public string Name { get; set; } // property. It is visible everywhere because the property is public 
+        
+
+        public int Age // this is the second way of doing a property
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
---
+++ Advanced Self Preparation/Defining Classes/Classes/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+           Person person = new Person();
+
+            person.Name = "Antoni";
+            person.Age = 29;
+
+            Console.WriteLine(person.Name);
+        }
+    }
+}
