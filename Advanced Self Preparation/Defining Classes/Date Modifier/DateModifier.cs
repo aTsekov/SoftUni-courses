e6434fc288b6fc44923f8040c3dd0d79c3aa5cbb
aTsekov <antoni.tsekov@gmail.com>
2022-05-13 19:18:37 +0200

    The problem is not solved I get 0 instead of a real value.

---
+++ Advanced Self Preparation/Defining Classes/Date Modifier/DateModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+        public static int CalculateDateDifferenceInDays (string firstDate, string secondDate)
+        {
+            DateTime dateOne = DateTime.Parse(firstDate);
+            DateTime dateTwo = DateTime.Parse(secondDate);
+
+            int days = (dateOne - dateOne).Days;
+
+            return days;
+        }
+
+    }
+}
---
+++ Advanced Self Preparation/Defining Classes/Date Modifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+            int days = DateModifier.CalculateDateDifferenceInDays(firstDate, secondDate);
+            Console.WriteLine(days);
+        }
+    }
+}
