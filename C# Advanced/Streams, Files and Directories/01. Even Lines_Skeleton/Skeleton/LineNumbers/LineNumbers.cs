b5a985a7161070b8712751ba3e820e56298a185d
aTsekov <antoni.tsekov@gmail.com>
2022-05-27 19:11:18 +0200

    To pass in Judge

--- C# Advanced/Streams, Files and Directories/01. Even Lines_Skeleton/Skeleton/LineNumbers/LineNumbers.cs
+++ C# Advanced/Streams, Files and Directories/01. Even Lines_Skeleton/Skeleton/LineNumbers/LineNumbers.cs
@@ -1,6 +1,8 @@
 ﻿namespace LineNumbers
 {
     using System;
+    using System.IO;
+
     public class LineNumbers
     {
         static void Main(string[] args)
@@ -13,7 +15,29 @@
 
         public static void ProcessLines(string inputFilePath, string outputFilePath)
         {
-            throw new NotImplementedException();
+            string inputPath = @"..\..\..\text.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            using (StreamReader input = new StreamReader(inputPath))
+            {
+                using (StreamWriter output = new StreamWriter(outputPath))
+                {
+                    string line = input.ReadLine();
+                    int count = 0;
+                    int num = 1;
+
+                    while (line != null)
+                    {
+
+                        output.WriteLine($"{num}.{line}");
+
+                        num++;
+                        count++;
+                        line = input.ReadLine();
+                    }
+
+                }
+            }
         }
     }
 }
