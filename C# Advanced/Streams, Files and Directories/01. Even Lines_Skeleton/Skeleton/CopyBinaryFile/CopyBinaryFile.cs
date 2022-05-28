d55f52d223e60ef13871ba94ec5e5a1dd162747a
aTsekov <antoni.tsekov@gmail.com>
2022-05-28 12:00:08 +0200

    Copy binary fail

--- C# Advanced/Streams, Files and Directories/01. Even Lines_Skeleton/Skeleton/CopyBinaryFile/CopyBinaryFile.cs
+++ C# Advanced/Streams, Files and Directories/01. Even Lines_Skeleton/Skeleton/CopyBinaryFile/CopyBinaryFile.cs
@@ -1,4 +1,7 @@
-﻿namespace CopyBinaryFile
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
 {
     public class CopyBinaryFile
     {
@@ -12,6 +15,27 @@
 
         public static void CopyFile(string inputFilePath, string outputFilePath)
         {
+            // Създаваме рийдър с който прочитам инпут файла
+            using(FileStream reader = new FileStream(inputFilePath,FileMode.Open))
+            {
+                //трябва ни writter - копира в outputfile
+
+                using (FileStream writer = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096]; // правим си буфер
+                        int countBytes = reader.Read(buffer, 0, buffer.Length); // на всяко завъртане записваме нови 4096 байта в масива
+                        if (countBytes == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, countBytes); // на всяко завъртане слагаме нови 4096 байта. 
+                    }
+                   
+                }
+            }
         }
     }
 }
