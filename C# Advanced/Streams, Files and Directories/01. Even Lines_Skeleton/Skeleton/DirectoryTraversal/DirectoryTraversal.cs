f14101e2bb5b9210e2b298fd6466ea7f00df9795
aTsekov <antoni.tsekov@gmail.com>
2022-05-28 12:45:39 +0200

    Traverse directory

--- C# Advanced/Streams, Files and Directories/01. Even Lines_Skeleton/Skeleton/DirectoryTraversal/DirectoryTraversal.cs
+++ C# Advanced/Streams, Files and Directories/01. Even Lines_Skeleton/Skeleton/DirectoryTraversal/DirectoryTraversal.cs
@@ -1,6 +1,11 @@
 ﻿namespace DirectoryTraversal
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
     public class DirectoryTraversal
     {
         static void Main(string[] args)
@@ -16,12 +21,48 @@
 
         public static string TraverseDirectory(string inputFolderPath)
         {
-            throw new NotImplementedException();
+            string[] files = Directory.GetFiles(inputFolderPath);
+            // разширение -> списък с файловете. 
+            Dictionary<string, List<FileInfo>> extentionInfo = new Dictionary<string, List<FileInfo>>();
+            // за всеки файл ни трябва разширението. 
+            foreach (var file in files)
+            {
+                // взимаме информацията за файла за да вземем разширението. 
+                FileInfo fileInfo = new FileInfo(file);
+                string extention = fileInfo.Extension;
+
+                if (!extentionInfo.ContainsKey(extention))
+                {
+                    extentionInfo.Add(extention, new List<FileInfo>());
+                }
+                extentionInfo[extention].Add(fileInfo);
+
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (var entry in extentionInfo.OrderByDescending(entry => entry.Value.Count).ThenBy(entry => entry.Key))
+            {
+                //взимаме на вс разширение списъка с файлове
+                //Списък с файлове трябва да се сортира спрямо размера на файла. 
+
+                string extention = entry.Key;
+                sb.Append(extention);
+                List<FileInfo> filesInfo = new List<FileInfo>();
+                filesInfo.OrderByDescending(file => file.Length);
+                foreach (FileInfo fileInfo in filesInfo)
+                {
+                    sb.Append(($"--{fileInfo.Name} - {fileInfo.Length / 1024:F3}kb"));
+                }
+            }
+
+            return sb.ToString();
         }
 
         public static void WriteReportToDesktop(string textContent, string reportFileName)
         {
-            throw new NotImplementedException();
+            // 
+            string pathReport = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+
+            File.WriteAllText(pathReport, textContent);
         }
 
     }
