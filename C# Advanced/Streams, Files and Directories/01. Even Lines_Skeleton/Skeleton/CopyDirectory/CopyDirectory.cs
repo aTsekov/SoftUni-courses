b9832a5a9dccbb434c9485348de30a6e1b45a66d
aTsekov <antoni.tsekov@gmail.com>
2022-05-28 11:05:30 +0200

    Copy files from one directory and move them to another directory

--- C# Advanced/Streams, Files and Directories/01. Even Lines_Skeleton/Skeleton/CopyDirectory/CopyDirectory.cs
+++ C# Advanced/Streams, Files and Directories/01. Even Lines_Skeleton/Skeleton/CopyDirectory/CopyDirectory.cs
@@ -1,6 +1,8 @@
 ﻿namespace CopyDirectory
 {
     using System;
+    using System.IO;
+
     public class CopyDirectory
     {
         static void Main(string[] args)
@@ -13,6 +15,18 @@
 
         public static void CopyAllFiles(string inputPath, string outputPath)
         {
+            //създаваме output директория
+            Directory.CreateDirectory(outputPath);
+            // взимаме вс файлове от инпут директорията 
+            string [] files = Directory.GetFiles(inputPath);
+            //3 всеки файл го копираме в output директорията:
+
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file); // взимаме името на файла
+                var copyDestionation = Path.Combine(outputPath, fileName); // в директорията на оутпут файла искаме да копираме файла, който взехме в предишната стъпка. 
+                File.Copy(file, copyDestionation); // копираме. 
+            }
         }
     }
 }
