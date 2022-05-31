c37edba5f1bfaaa40772a3c8b2ac45ebd68ad045
aTsekov <antoni.tsekov@gmail.com>
2022-05-31 22:17:51 +0200

    Solved with LINQ and without predicate

---
+++ C# Advanced/Functional Programming/11 TriFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11_TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Console.WriteLine(names
+                .First(name => name.Select(symbol => (int) symbol).Sum() >= n));
+
+            //Print the first name > Select each symbol of that name cast it to int value of each symbol and the sum should be = n
+            
+        }
+    }
+}
