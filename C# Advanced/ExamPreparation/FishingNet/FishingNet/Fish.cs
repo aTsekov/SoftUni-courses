09a7a1056ab64052774f82a2e958bad6ed406f43
aTsekov <antoni.tsekov@gmail.com>
2022-06-06 23:24:14 +0200

    I should fix the printing methods

---
+++ C# Advanced/ExamPreparation/FishingNet/FishingNet/Fish.cs
@@ -0,0 +1,36 @@
+﻿namespace FishingNet
+{
+    public class Fish
+    {
+
+        private string _FishType;
+        private double _Lenght;
+        private double _Weight;
+        public Fish(string fishType, double lenght, double weight)
+        {
+            _FishType = fishType;
+            _Lenght = lenght;
+            _Weight = weight;
+        }
+        public string FishType
+        {
+            get { return _FishType; }
+            set { _FishType = value; }
+        }
+        public double Lenght
+        {
+            get { return _Lenght; }
+            set { _Lenght = value; }
+        }
+        public double Weight
+        {
+            get { return _Weight; }
+            set { _Weight = value; }
+        }
+        public override string ToString()
+        {
+            return ($"There is a {FishType}, {Lenght} cm. long, and {Weight} gr. in weight.");
+        }
+
+    }
+}
---
+++ C# Advanced/ExamPreparation/FishingNet/FishingNet/Net.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        public List<Fish> Fish { get; set; }
+        public string Matrial { get; set; }
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get { return Fish.Count; }
+        }
+
+
+        public Net(string material, int capacity)
+        {
+            Fish = new List<Fish>();
+            Capacity = capacity;
+            Matrial = material;
+        }
+
+        public string AddFish(Fish fish)
+        {
+            if (fish.FishType == null || fish.FishType == " " || fish.Lenght <= 0 || fish.Weight <= 0)
+            {
+                return "Invalid fish.";
+            }
+            if (Capacity <= Count)
+            {
+                return "Fishing net is full.";
+            }
+            else
+            {
+                Fish.Add(fish);
+                return $"Successfully added {fish.FishType} to the fishing net.";
+            }
+        }
+        //bool ReleaseFish(double weight) – removes a fish by given weight, if such exists return true, otherwise false.
+        public bool ReleaseFish(double weight)
+        {
+
+            if (Fish.Any(f => f.Weight ==weight))
+            {
+                Fish.RemoveAll(f => f.Weight == weight);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Fish GetFish(string fishType) – search and returns a fish by given fish type.
+        public string GetFish (string fishType)
+        {
+            if (Fish.Any(f => f.FishType == fishType))
+            {
+                return fishType;
+            }
+            else
+            {
+                return null;
+            }
+             
+        }
+        public Fish GetBiggestFish()
+        {
+            double longestFish = 0;
+            foreach (var fish in Fish)
+            {
+                if (fish.Lenght > longestFish)
+                {
+                    longestFish = fish.Lenght;
+                }
+            }
+            return Fish.Where(f=> f.Lenght == longestFish).FirstOrDefault();
+        }
+        //Report() - returns information about the Net and all fish that were not released, order by fish's length descending  in the following format:
+        public string Report()
+        {
+            var sortedFishes = Fish.OrderByDescending(f => f.Lenght).ToList();
+            
+            foreach (var fish in sortedFishes)
+            {
+                return $"Into the {Matrial}:{Environment.NewLine}There is a {fish.FishType}, {fish.Lenght} cm. long, and {fish.Weight} gr. in weight.";
+
+            }
+            return null;
+        }
+    }
+}
