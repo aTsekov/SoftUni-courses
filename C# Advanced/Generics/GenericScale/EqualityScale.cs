b106fc4444281607c1db69cac5dbdb352eac4ac1
aTsekov <antoni.tsekov@gmail.com>
2022-06-08 19:34:42 +0200

    Use of object class method Equals

---
+++ C# Advanced/Generics/GenericScale/EqualityScale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T _left;
+        private T _right;
+        public EqualityScale(T left, T right)
+        {
+            this._left = left;
+            this._right = right;
+        }
+        public bool AreEqual()
+        {
+            if (_left.Equals(_right))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ C# Advanced/Generics/GenericScale/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericScale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
