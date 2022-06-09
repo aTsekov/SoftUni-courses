dba8e4e7473366093042c5703b247c4c401c3e6c
aTsekov <antoni.tsekov@gmail.com>
2022-06-09 17:35:25 +0200

    RandomList

---
+++ C# Advanced/Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public Random Random { get; set; }
+        public RandomList()
+        {
+            Random = new Random();
+        }
+        public string RandomString()
+        {
+            string stringToReturn = string.Empty;
+            int index = Random.Next(0, this.Count);
+            stringToReturn = this[index];
+            this.RemoveAt(index);
+            return stringToReturn;
+            
+        }
+
+    }
+}
---
+++ C# Advanced/Inheritance/CustomRandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList mylist = new RandomList();
+
+            mylist.Add("Gosho");
+            mylist.Add("Pesho");
+            mylist.Add("Maria");
+            mylist.Add("Ganka");
+            mylist.RandomString();
+        }
+    }
+}
