1994578b78e8880c5e981c30c0b102101c920f73
aTsekov <antoni.tsekov@gmail.com>
2022-06-09 17:51:21 +0200

    Stack of Strings inherit base class Stack<string>

---
+++ C# Advanced/Inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0) // if the count of the stack is 0 then return true
+            {
+                return true;
+            } 
+            return false;
+        }
+        public void AddRange(IEnumerable<string> elements) // It takes data sctructure of elements because we are using ForEach. IEnumerable is a base type. This way we can use it on every type of Data structure such as List, Stack, Queue, Array, etc. 
+        {
+            foreach (var item in elements) // for each item in elements push the item to the stack, list, array, etc. 
+            {
+                Push(item);
+            }
+        }
+
+    }
+}
---
+++ C# Advanced/Inheritance/CustomStack/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> test = new List<string>();
+            test.Add("Pesho");
+            test.Add("Gosho");
+            test.Add("Maria");
+            test.Add("Ganka");
+            var myStack = new StackOfStrings();
+            Console.WriteLine(myStack.IsEmpty());
+            myStack.AddRange(test);
+        }
+    }
+}
