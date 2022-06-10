1232e25ede94a31a783ffdb49792964d7d92b58e
aTsekov <antoni.tsekov@gmail.com>
2022-06-10 17:07:33 +0200

    Generic Count Method Strings

---
+++ C# Advanced/Generics/Box1/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using Genereics;
+using System;
+using System.Collections.Generic;
+
+namespace Box1
+{
+    public class StarUp
+    {
+        static void Main(string[] args)
+        {
+            
+            var numberOfLine = int.Parse(Console.ReadLine());
+            
+
+            // Task 5 and 6
+            var list = new List<string>();
+            for (int i = 0; i < numberOfLine; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            var box = new Box<string>(list);
+            var elementToCompare = Console.ReadLine();
+            var count = box.CountOFGreaterElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
