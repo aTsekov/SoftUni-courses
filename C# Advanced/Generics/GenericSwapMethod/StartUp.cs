ef36dff887a62b0fa18a2c5a5393bd1ea185b9a3
aTsekov <antoni.tsekov@gmail.com>
2022-06-10 16:55:22 +0200

    Generic Swap Method of Generic List

---
+++ C# Advanced/Generics/GenericSwapMethod/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethod
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+               list.Add(input); 
+            }
+
+            int[] indices = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var swap = new Swap<string>(list);
+            swap.SwapMethod(list,indices[0],indices[1]);
+            Console.WriteLine(swap);
+        }
+    }
+}
---
+++ C# Advanced/Generics/GenericSwapMethod/Swap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericSwapMethod
+{
+    public class Swap<T>
+    {
+        public List<T> MyList { get; set; }
+
+        public Swap(List<T> myList)
+        {
+            this.MyList = myList;
+        }
+
+        public void SwapMethod(List<T> myList, int index1, int index2)
+        {
+            T fistElement = MyList[index1];
+            myList[index1] = MyList[index2];
+            myList[index2] = fistElement;
+        }
+        public override string ToString() // overrive ToString in order to print the instance of Box directly in the format we want.
+        {
+            var sb = new StringBuilder();
+
+            foreach (T element in MyList)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
