435a63a90a89d62c77e85a116243be8502c19d5f
aTsekov <antoni.tsekov@gmail.com>
2022-06-10 16:58:17 +0200

    GenericSwapMethod for generic list but with INT

--- C# Advanced/Generics/GenericSwapMethod/StartUp.cs
+++ C# Advanced/Generics/GenericSwapMethod/StartUp.cs
@@ -9,17 +9,17 @@ namespace GenericSwapMethod
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            var list = new List<string>();
+            var list = new List<int>();
 
             for (int i = 0; i < n; i++)
             {
-                string input = Console.ReadLine();
+                int input = int.Parse (Console.ReadLine());
                list.Add(input); 
             }
 
             int[] indices = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
-            var swap = new Swap<string>(list);
+            var swap = new Swap<int>(list);
             swap.SwapMethod(list,indices[0],indices[1]);
             Console.WriteLine(swap);
         }
