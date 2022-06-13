a6849418adb226859074b3b44ac1af04a586a55e
aTsekov <antoni.tsekov@gmail.com>
2022-06-13 20:23:09 +0200

    Iterators

---
+++ C# Advanced/Iterators and Comparators/Library/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year, params string [] authors) // params string [] is used to be able to add a few authors at the same time.
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+        }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+
+    }
+}
---
+++ C# Advanced/Iterators and Comparators/Library/Library.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book [] books)
+        {
+            this.books = new List<Book>(books);
+        }
+        public List<Book> Books { get; set; }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
