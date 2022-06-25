ae902935be9bb3a5841640a170822429bb193144
aTsekov <antoni.tsekov@gmail.com>
2022-06-25 11:33:56 +0200

    Exam Task 3 80/100

---
+++ C# Advanced/ExamPreparation/Renovations/Catalog.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        public Catalog(string nameCatalog, int neededRenovators, string project)
+        {
+            RenovatorsList = new List<Renovator>();
+            this.NameCatalog = nameCatalog;
+            this.NeededRenovators = neededRenovators;
+            this.Project = project;
+        }
+        public List<Renovator> RenovatorsList { get; set; }
+        public string NameCatalog { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+        public int Count
+        {
+            get { return this.RenovatorsList.Count; }
+        }
+
+        public string AddRenovator(Renovator renovator)
+        {
+            if (string.IsNullOrWhiteSpace(renovator.Name) || string.IsNullOrWhiteSpace(renovator.Type))
+            {
+                return $"Invalid renovator's information.";
+            }
+            else if (RenovatorsList.Count == NeededRenovators)
+            {
+                return $"Renovators are no more needed.";
+            }
+            else if (renovator.Rate > 350) // should be =???
+            {
+                return $"Invalid renovator's rate.";
+            }
+            else
+            {
+                RenovatorsList.Add(renovator);
+                return $"Successfully added {renovator.Name} to the catalog.";
+            }
+        }
+        public bool RemoveRenovator(string name)
+        {
+            if (RenovatorsList.Any(r => r.Name == name))
+            {
+                RenovatorsList.RemoveAll(r => r.Name == name);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            if (RenovatorsList.Any(r => r.Type == type))
+            {
+                int countOfRemoved = RenovatorsList.Count(r => r.Type == type); // is this way of getting the count a mistake? 
+                RenovatorsList.RemoveAll(r => r.Type == type);
+                return countOfRemoved;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public Renovator HireRenovator(string name)
+        {
+            if (RenovatorsList.Any(r => r.Name == name))
+            {
+                Renovator reno = RenovatorsList.FirstOrDefault(r => r.Name == name);
+                reno.Hired = true;
+                return reno;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public List<Renovator> PayRenovators(int days)
+        {
+            var workingRenos = RenovatorsList.FindAll(r => r.Days >= days);
+            return workingRenos;
+        }
+        public string Report()
+        {
+            var availableRenos = RenovatorsList.Where(d => d.Hired == true);
+
+            return $"Renovators available for Project {Project}:{Environment.NewLine}{string.Join(Environment.NewLine, availableRenos)}";
+        }
+
+    }
+}
---
+++ C# Advanced/ExamPreparation/Renovations/Renovator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    { 
+
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Rate = rate;
+            this.Days = days;
+
+        }
+        private string _name;
+        private string _type;
+        private double _rate;
+        private int _days;
+        private bool _hired = false;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+        public double Rate
+        {
+            get { return _rate; }
+            set { _rate = value; }
+        }
+        public int Days
+        {
+            get { return _days; }
+            set { _days = value; }
+        }
+        public bool Hired
+        {
+            get { return _hired; }
+            set { _hired = value; }
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"-Renovator: {Name}{Environment.NewLine}--Specialty: {Type}{Environment.NewLine}--Rate per day: {Rate} BGN";
+        }
+    }
+}
