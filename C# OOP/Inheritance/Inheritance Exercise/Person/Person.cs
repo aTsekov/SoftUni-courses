62c5446c6cd67246522a36cf40331430e9ed991a
aTsekov <antoni.tsekov@gmail.com>
2022-06-27 18:53:48 +0200

    Inheritance Exercise

---
+++ C# OOP/Inheritance/Inheritance Exercise/Person/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age  )
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public  int Age
+
+        {
+            get { return this._age; }
+            set
+            {
+                if (value < 0) // If the value of the years is smaller than 0 >> throw exeption.
+                {
+                    throw new Exception();
+                }
+                //else
+                this._age=value;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(string.Format("Name: {0}, Age: {1}",this.Name,this.Age));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
