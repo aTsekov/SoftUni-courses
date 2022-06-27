6429d5d40dc952c899b7de46b37096ee0db84975
aTsekov <antoni.tsekov@gmail.com>
2022-06-27 22:01:33 +0200

    Default values for each class.

---
+++ C# OOP/Inheritance/Inheritance Exercise/NeedForSpeed/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        private const double DefaultFuelConsumption = 3; //Here we state that the default fuel consumption is 3. THen we override the value of FuelConsumption because it is virtual poperty. 
+        public Car(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+        public override double FuelConsumption => DefaultFuelConsumption; 
+    }
+}
---
+++ C# OOP/Inheritance/Inheritance Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+
+            FuelConsumption = DefaultFuelConsumption;
+        }
+     
+        public virtual double FuelConsumption { get; set; } 
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * FuelConsumption; // /100?
+        }
+    }
+}
