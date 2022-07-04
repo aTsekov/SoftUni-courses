507047c4ae51fde5a0262afbfcc6f5bfd3f9b505
aTsekov <antoni.tsekov@gmail.com>
2022-07-04 20:42:19 +0200

    Encapsulation

---
+++ C# OOP/Encapsulation/FirstAndReserveTeam/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private int _age;
+        private string firstName;
+        private string lastName;
+
+
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get => this._age;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                _age = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives ";
+        }
+    }
+}
