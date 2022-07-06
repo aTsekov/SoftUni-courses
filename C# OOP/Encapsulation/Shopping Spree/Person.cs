2f9e7cb995b8d697a3e4f7260701b57c903f4cec
aTsekov <antoni.tsekov@gmail.com>
2022-07-06 18:03:31 +0200

    100/100

---
+++ C# OOP/Encapsulation/Shopping Spree/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shopping_Spree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly List <Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.Products = new List <Product>();
+            
+        }
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        public decimal Money
+        {
+            get { return money; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+        public List<Product> Products { get; set; }
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+        public void ReduceMoney(Product product)
+        {
+            money -= product.Cost;
+        }
+        public override string ToString()
+        {
+            if (this.Products.Count > 0)
+            {
+                return $"{this.Name} - {string.Join(", ", Products.Select(p => p.Name))}";
+            }
+            else
+            {
+                return $"{this.Name} - Nothing bought";
+            }
+        }
+    }
+
+}
---
+++ C# OOP/Encapsulation/Shopping Spree/StartUp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping_Spree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+
+                List<Person> people = new List<Person>();
+
+                string[] peopleInfo = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var person in peopleInfo)
+                {
+                    string[] parts = person.Split('=');
+                    string name = parts[0];
+                    decimal value = decimal.Parse(parts[1]);
+                    Person person1 = new Person(name, value);
+
+                    people.Add(person1);
+                }
+
+                List<Product> products = new List<Product>();
+
+                string[] productInfo = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var product in productInfo)
+                {
+                    string[] parts = product.Split('=');
+                    string productName = parts[0];
+                    decimal productValue = decimal.Parse(parts[1]);
+                    Product productProduct = new Product(productName, productValue);
+                    products.Add(productProduct);
+                }
+
+                string command;
+                while ((command = Console.ReadLine()) != "END")
+                {
+                    string[] tokens = command.Split(' ');
+                    string personName = tokens[0];
+                    string productName = tokens[1];
+
+                    var currentPerson = people.FirstOrDefault(p => p.Name == personName);
+                    var currnetProduct = products.FirstOrDefault(p => p.Name == productName);
+                    if (currentPerson.Money >= currnetProduct.Cost)
+                    {
+                        currentPerson.AddProduct(currnetProduct);
+                        currentPerson.ReduceMoney(currnetProduct);
+                        Console.WriteLine($"{currentPerson.Name} bought {currnetProduct.Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{currentPerson.Name} can't afford {currnetProduct.Name}");
+                    }
+                }
+
+                foreach (var person in people)
+                {
+                    Console.WriteLine(person);
+                }
+            }
+            catch (Exception ae)
+            {
+                Console.WriteLine(ae.Message);
+
+            }
+
+
+        }
+    }
+}
