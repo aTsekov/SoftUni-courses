fe32d583873bb1dab34ec45527acab2f1a84cb4e
aTsekov <antoni.tsekov@gmail.com>
2022-07-11 19:36:20 +0200

    100/100

---
+++ C# OOP/Polymorphism/Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+        public Animal(string name, string faviouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = faviouriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my faviourite food is {this.FavouriteFood}";
+        }
+
+    }
+}
---
+++ C# OOP/Polymorphism/Animals/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string faviouriteFood) : base(name, faviouriteFood)
+        {
+        }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "MEEOW";
+        }
+    }
+}
