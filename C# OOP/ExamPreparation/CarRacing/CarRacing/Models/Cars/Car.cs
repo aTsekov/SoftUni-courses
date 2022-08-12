1de81205a7eb04e6c940c552196648abf71d8762
aTsekov <antoni.tsekov@gmail.com>
2022-08-12 17:55:44 +0200

    To be solved

---
+++ C# OOP/ExamPreparation/CarRacing/CarRacing/Models/Cars/Car.cs
@@ -0,0 +1,110 @@
+﻿namespace CarRacing.Models.Cars
+{
+    using CarRacing.Models.Cars.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class Car : ICar
+    {
+        private string make;
+        private string model;
+        private string vin;
+        private int horsePower;
+        private double fuelAvailable;
+        private double fuelConsumptionPerRace;
+        public Car(string make, string model, string VIN, int horsePower, double fuelAvailable, double fuelConsumptionPerRace)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.VIN = vin;
+            this.HorsePower = horsePower;
+            this.FuelAvailable = fuelAvailable;
+            this.FuelConsumptionPerRace = fuelConsumptionPerRace;
+        }
+        public string Make
+        {
+            get { return make; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Car make cannot be null or empty.");
+                }
+                make = value;
+            }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Car model cannot be null or empty.");
+                }
+                model = value;
+            }
+        }
+
+        public string VIN
+        {
+            get { return vin; }
+            private set
+            {
+                if (value.Length != 17)
+                {
+                    throw new ArgumentException("Car VIN must be exactly 17 characters long.");
+                }
+                vin = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Horse power cannot be below 0.");
+                }
+                horsePower = value;
+            }
+        }
+
+        public double FuelAvailable
+        {
+            get { return fuelAvailable; }
+            private set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                fuelAvailable = value;
+            }
+        }
+
+        public double FuelConsumptionPerRace
+        {
+            get { return fuelConsumptionPerRace; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Fuel consumption cannot be below 0.");
+                }
+                fuelConsumptionPerRace = value;
+            }
+        }  
+
+
+        public virtual void Drive()
+        {
+            this.FuelAvailable -= this.FuelConsumptionPerRace;
+            
+        }
+    }
+}
