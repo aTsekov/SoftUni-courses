5b1f5b11edf4a562465c8b53d4e30deef32f5513
aTsekov <antoni.tsekov@gmail.com>
2022-09-09 13:17:23 +0200

    Simple function

---
+++ JS Advanced/Syntax, Functions and Statements/Echo Function.js
@@ -0,0 +1,12 @@
+
+let as = 'Hello, JavaScript!'
+myFunction(as);
+
+function myFunction (a){ // this is equivalent to a method in C#
+
+    let len = a.length;
+    console.log(len);
+    console.log(a);
+    
+}
+
