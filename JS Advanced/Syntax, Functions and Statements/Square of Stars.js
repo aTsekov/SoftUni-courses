0a727f1b4c68787796e23350556a160784d9b103
aTsekov <antoni.tsekov@gmail.com>
2022-09-09 16:48:20 +0200

    Refactoring

--- JS Advanced/Syntax, Functions and Statements/Square of Stars.js
+++ JS Advanced/Syntax, Functions and Statements/Square of Stars.js
@@ -3,13 +3,9 @@ function sq (a){
     if(typeof a != 'number'){
          a = 5;
     }
-    
+        for(let j = 0; j < a; j++){
+            console.log(`${('* '.repeat(a)).trimEnd()}`);
+        }
 
-    for(let j = 0; j < a; j++){
-        console.log(`${('* '.repeat(a)).trimEnd()}`);
-    }
-    
-    
 }
-
 sq(3);
