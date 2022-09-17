de5ab6100630df477f773a6028b7bb225f7dd00a
aTsekov <antoni.tsekov@gmail.com>
2022-09-17 12:17:39 +0200

    refactoring

--- JS Advanced/Arrays and Nested Arrays/List of Name.js
+++ JS Advanced/Arrays and Nested Arrays/List of Name.js
@@ -1,5 +1,6 @@
 function sortNames(arr){
-    arr.sort();
+    arr.sort((a,b) => a.localeCompare(b));// sort names. With localeCompare we make sure that small/capital letters are  disregarded.
+
     for (let i = 0; i < arr.length; i++) {
         arr[i] = i+1 + '.' + arr[i]
         
