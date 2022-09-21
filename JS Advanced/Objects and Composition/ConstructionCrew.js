99fd3236eeec4706adc2ee9d657be82edc4ff648
aTsekov <antoni.tsekov@gmail.com>
2022-09-21 19:14:50 +0200

    Modification of obj

---
+++ JS Advanced/Objects and Composition/ConstructionCrew.js
@@ -0,0 +1,37 @@
+function workerFunc(dataObj) {
+    let result = {}
+    result.weight = dataObj.weight;
+    result.experience = dataObj.experience;
+    result.levelOfHydrated = dataObj.experience;
+    result.dizziness = dataObj.dizziness;
+
+    if(result.dizziness){
+        let waterToAdd = 0.1 * result.weight * result.experience;
+        result.levelOfHydrated += waterToAdd;
+        result.dizziness = false;
+    }
+   
+        console.log(result)
+    return result;  
+    
+
+}
+
+workerFunc({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+});
+workerFunc({
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true
+});
+workerFunc({
+    weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false
+});
