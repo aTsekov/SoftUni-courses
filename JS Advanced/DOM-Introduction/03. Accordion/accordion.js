98ed8e36fc980804793be7acd3d1b592a5a79312
aTsekov <antoni.tsekov@gmail.com>
2022-09-24 12:18:08 +0200

    More/Less text on a page

--- JS Advanced/DOM-Introduction/03. Accordion/accordion.js
+++ JS Advanced/DOM-Introduction/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
-function toggle() {
-    console.log('TODO:...');
-}
+ 
+    function toggle() {
+      let button = document.getElementsByClassName("button")[0];
+      
+      let divElement = document.getElementById("extra")
+
+      if(button.textContent == 'More') {
+        divElement.style.display = 'block';
+        button.textContent = 'Less';
+      }
+      else if(button.textContent == 'Less') {
+        divElement.style.display = 'none';
+        button.textContent = 'More';
+      }
+    }
--- JS Advanced/JSnotes.js
+++ JS Advanced/JSnotes.js
@@ -3,3 +3,16 @@ document.getElementById('firstNumber').value; // Select the element with ID "fir
 document.getElementById('result').innerText = result // The innerText property sets or returns the text content of an element.(in this case
 // an element with ID "result")
 
+document.getElementsByClassName("button")[0]; //hat returns a collection and we need only one element from it, 
+//so the correct way is to use getElementsByClassName("button")[0] as it will return the needed span element. span.button
+
+//This selects the TAG <span> with class button. Another way to do the same as the row above. 
+document.querySelector('span.button');
+<span class="button" onclick="toggle()">More</span>;
+
+// The display property specifies if/how an element is displayed. Elements in HTML are mostly "inline" or "block" elements:
+// An inline element has floating content on its left and right side. A block element fills the entire line,
+// and nothing can be displayed on its left or right side. (divElement is a variable)
+divElement.style.display = 'none';
+divElement.style.display = 'block';
+
