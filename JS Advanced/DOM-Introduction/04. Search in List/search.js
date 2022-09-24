a4d16050227bbb0c62eeaf8d4180172a53351b85
aTsekov <antoni.tsekov@gmail.com>
2022-09-24 12:53:13 +0200

    Search in list of items

--- JS Advanced/DOM-Introduction/04. Search in List/search.js
+++ JS Advanced/DOM-Introduction/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+
+   let towns = Array.from(document.querySelectorAll("#towns li"));
+   let textToSearch = document.getElementById('searchText').value;
+
+
+   let count = 0;
+
+   for (let town of towns) {
+      let text = town.textContent;
+
+      if (text.includes(textToSearch)) {
+         town.style.textDecoration = "underline";
+         town.style.fontWeight = "bold";
+         count++;
+      }
+      else {
+         town.style.textDecoration = "none";
+         town.style.fontWeight = "";
+      }
+
+   }
+
+   document.getElementById("result").innerHTML = `${count} matches found`;
 }
