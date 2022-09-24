76d5b6498496d4166879e62e7399c0275948c734
aTsekov <antoni.tsekov@gmail.com>
2022-09-24 11:22:45 +0200

    Transfor text in to Pascal or Camel case

--- JS Advanced/DOM-Introduction/02. Pascal or Camel Case/solution.js
+++ JS Advanced/DOM-Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
-function solve() {
-  //TODO...
+ function solve() {
+      let input = document.getElementById('text').value;
+      let command = document.getElementById('naming-convention').value;
+      let place = document.getElementById('result');
+
+      let resultArr = input.toLowerCase().split(' ');
+      let resultStr ="";
+
+      for (let i = 0; i < resultArr.length; i++) {
+            
+            let currentWord = resultArr[i];
+            if (command === "Camel Case") {
+                  
+                  if(i === 0) {
+                        resultStr += currentWord;
+                  }
+                  else{
+                        resultStr += currentWord[0].toUpperCase() + currentWord.substring(1)
+                  }
+            }
+            else if (command === "Pascal Case") {
+                  resultStr += currentWord[0].toUpperCase() + currentWord.substring(1);
+            }
+            else{
+                  resultStr = 'Error!';
+            }
+      }
+      place.innerText = resultStr;
+
 }
---
+++ JS Advanced/JSnotes.js
@@ -0,0 +1,5 @@
+document.getElementById('firstNumber').value; // Select the element with ID "firstNumber" and take its value
+
+document.getElementById('result').innerText = result // The innerText property sets or returns the text content of an element.(in this case
+// an element with ID "result")
+
