30e55046d7b0a9111632f58d76ae0a3ff2d9b7c9
aTsekov <antoni.tsekov@gmail.com>
2022-09-25 13:40:51 +0200

    Format text

--- JS Advanced/DOM-Introduction/06. Format the Text/solution.js
+++ JS Advanced/DOM-Introduction/06. Format the Text/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO
+
+  let input = document.getElementById('input').value;
+  let output = document.getElementById('output');
+
+  let resultArr = input.split('.');
+  resultArr.pop();
+
+  let counter = 0;
+  let threeSentences = '';
+  let flag = false;
+  for (let i = 0; i < resultArr.length; i++) {
+
+
+    if (counter <= 3) {
+      threeSentences += resultArr[i] + '. ';
+      counter++;
+      if (counter === 3 || resultArr.length === i + 1) {
+        flag = true;
+        counter = 0;
+      }
+    }
+
+
+    if (flag === true) {
+      let res = `<p>${threeSentences.trimEnd()}</p>`;
+      output.innerHTML += res;
+      res = '';
+      threeSentences = "";
+      flag = false;
+
+    }
+
+  }
+
 }
