c99fdee9d58b603fa18cc1ebc02f87a2279d38cf
aTsekov <antoni.tsekov@gmail.com>
2022-09-26 19:22:19 +0200

    To seek for solution

--- JS Advanced/DOM-Introduction/08. Generate Report/report.js
+++ JS Advanced/DOM-Introduction/08. Generate Report/report.js
@@ -1,3 +1,25 @@
 function generateReport() {
-    //TODO
+    let tableRows = Array.from(document.querySelectorAll('tbody tr')); 
+    let columnName = Array.from(document.querySelectorAll('thead tr'));
+    let checkboxes = Array.from(document.querySelectorAll('input[type=checkbox]'))
+    let myObj = {};
+    
+
+        for (const checkbox of checkboxes) {
+            if(checkbox.checked){
+
+                let propertyName = checkbox.name;
+
+                for (const r of tableRows){
+                    obj = {};
+                    obj = r.textContent;
+                    console.log(obj);
+                    myObj.propertyName = obj[propertyName];
+                    console.log(myObj)
+                }
+
+            }
+        }
+        
+    
 }
