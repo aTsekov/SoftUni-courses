8fa5e167abe542cc62cf1735d505795f8ea1d6f7
aTsekov <antoni.tsekov@gmail.com>
2022-09-30 16:12:15 +0200

    Create new options via DOM

--- JS Advanced/DOM events/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/DOM events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    
+    let  itemText = document.getElementById("newItemText").value;
+    let itemValue = document.getElementById("newItemValue").value;
+    let option = document.createElement('option');
+    option.textContent = itemText;
+    option.value = itemValue;
+
+    let menu = document.getElementById('menu');
+    menu.appendChild(option);
+
+let empty ='';
+
+    document.getElementById("newItemText").value = empty;
+    document.getElementById("newItemValue").value = empty;
 }
--- JS Advanced/JSnotes.js
+++ JS Advanced/JSnotes.js
@@ -55,3 +55,37 @@ for (const row of tableRows) {
 //get all of the buttons by input type. 
 let buttons = Array.from(document.querySelectorAll("input[type=button]"));
 
+
+// Show or Hide information if the radio buttons are locked or unlocked. 
+function lockedProfile() {
+
+   let showMoreButtons = Array.from(document.getElementsByTagName("button"));
+
+   for (let button of showMoreButtons) {
+       button.addEventListener("click", showMore);
+   }
+   function showMore(event) {
+
+       let but = event.target; // this is the button "Show more" or "Hide it". 
+       let profile = but.parentNode; // this is the div element with class "profile". 
+       let moreInformation = profile.getElementsByTagName('div')[0]; // this is the div element with ID user1HiddenFields or user2HiddenFields etc.
+       let lockUnclockButtons = profile.querySelector("input[type=radio]:checked").value; //This shows if the radio button in the big DIV element 
+       // with class "profile" is locked or not. 
+
+       if (lockUnclockButtons === "unlock") {
+           if (but.textContent === "Show more") {
+               moreInformation.style.display = "inline-block"
+               event.target.innerText = "Hide it";
+           }
+           else {
+               moreInformation.style.display = "none";
+               event.target.innerText = "Show more";
+           }
+
+       }
+       
+
+   }
+
+}
+
