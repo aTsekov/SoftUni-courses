69014282cd69d42f98cf3ec518af5e9ae8accd75
aTsekov <antoni.tsekov@gmail.com>
2022-09-30 15:48:03 +0200

    Solved and explanations added.

--- JS Advanced/DOM events/03. Locked-Profile/app.js
+++ JS Advanced/DOM events/03. Locked-Profile/app.js
@@ -1,34 +1,31 @@
 function lockedProfile() {
 
-    let lockUnclockButtons = Array.from(document.querySelectorAll("input[type=radio]"));
     let showMoreButtons = Array.from(document.getElementsByTagName("button"));
 
-    for (const lockBut of lockUnclockButtons) {
-        lockBut.addEventListener("click", lock);
-
-        function lock(event) {
-
-            if (lockBut.value === "unlock") {
-                for (let button of showMoreButtons) {
-                    button.addEventListener("click", showMore);
-                }
-
-            }
-        }
-
+    for (let button of showMoreButtons) {
+        button.addEventListener("click", showMore);
     }
     function showMore(event) {
-        let user1Div = document.getElementById("user1HiddenFields");
-        let but = event.target.innerText;
 
-        if (but === "Show more") {
-            user1Div.style.display = "inline-block"
-            event.target.innerText = "Hide It";
+        let but = event.target; // this is the button "Show more" or "Hide it". 
+        let profile = but.parentNode; // this is the div element with class "profile". 
+        let moreInformation = profile.getElementsByTagName('div')[0]; // this is the div element with ID user1HiddenFields or user2HiddenFields etc.
+        let lockUnclockButtons = profile.querySelector("input[type=radio]:checked").value; //This shows if the radio button in the big DIV element 
+        // with class "profile" is locked or not. 
+
+        if (lockUnclockButtons === "unlock") {
+            if (but.textContent === "Show more") {
+                moreInformation.style.display = "inline-block"
+                event.target.innerText = "Hide it";
+            }
+            else {
+                moreInformation.style.display = "none";
+                event.target.innerText = "Show more";
+            }
+
         }
-        else {
-            user1Div.style.display = "none";
-            event.target.innerText = "Show more";
-        }
+        
 
     }
+
 }
