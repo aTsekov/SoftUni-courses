374f06b1abbf204cc2b40df0cc1b2fa0a0f4c9ff
aTsekov <antoni.tsekov@gmail.com>
2022-10-01 11:10:42 +0200

    25/100 - works correctly but Judge does not accept it. To seek for errors

--- JS Advanced/DOM events/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/DOM events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,42 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    
+    let button = document.addEventListener('click', transform);
+
+
+    function transform (event) {
+        let but = event.target;
+        
+        let textElements = Array.from(document.getElementsByTagName('textarea'));
+
+        let stringToTransform;
+        let result ="";
+
+        //debugger;
+        if(but.textContent === "Encode and send it"){
+            stringToTransform = textElements[0].value;
+
+            for (let i = 0; i < stringToTransform.length; i++) {
+                let charNum = stringToTransform.charCodeAt(i);
+                charNum += 1;
+                result += String.fromCharCode(charNum);
+                
+            }
+            textElements[0].value = "";
+            textElements[1].value = result;
+            let test = textElements[1].value
+            console.log(test);
+
+        }
+        else if(but.textContent ==="Decode and read it"){
+            stringToTransform = textElements[1].value;
+
+            for (let i = 0; i < stringToTransform.length; i++) {
+                let charNum = stringToTransform.charCodeAt(i);
+                charNum -= 1;
+                result += String.fromCharCode(charNum);
+                
+            }
+            textElements[1].value = result;
+        }
+    }
 }
