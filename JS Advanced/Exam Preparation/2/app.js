9561fea6a22a846445369f68d604c50b09804e6f
aTsekov <antoni.tsekov@gmail.com>
2022-10-19 14:58:14 +0200

    100/100

--- JS Advanced/Exam Preparation/2/app.js
+++ JS Advanced/Exam Preparation/2/app.js
@@ -24,25 +24,25 @@ function solve() {
                   <td>${originalCost.value}</td>
                   <td>${sellingPrice.value}</td>`;
 
-                  let buttonsTD = document.createElement('td');
-                 
-                  let editButton = document.createElement('button');
-                  editButton.classList.add('action-btn');
-                  editButton.classList.add('edit');
-                  editButton.innerText = `Edit`;
+      let buttonsTD = document.createElement('td');
 
-                  let sellButton = document.createElement('button');
-                  sellButton.classList.add('action-btn');
-                  sellButton.classList.add('sell');
-                  sellButton.innerText = `Sell`;
-                  buttonsTD.appendChild(editButton);
-                  buttonsTD.appendChild(sellButton);
-                  tr.appendChild(buttonsTD)
-                  ;
+      let editButton = document.createElement('button');
+      editButton.classList.add('action-btn');
+      editButton.classList.add('edit');
+      editButton.innerText = `Edit`;
+
+      let sellButton = document.createElement('button');
+      sellButton.classList.add('action-btn');
+      sellButton.classList.add('sell');
+      sellButton.innerText = `Sell`;
+      buttonsTD.appendChild(editButton);
+      buttonsTD.appendChild(sellButton);
+      tr.appendChild(buttonsTD)
+        ;
 
       let tbody = document.getElementById("table-body");
       tbody.appendChild(tr);
-      
+
       make.value = "";
       model.value = "";
       productionYear.value = "";
@@ -50,49 +50,55 @@ function solve() {
       originalCost.value = "";
       sellingPrice.value = "";
 
+      //It is very important where exactly the event listener is placed/ 
+      //Additionally by adding the event listener to the button basically I will add it to all buttons of the same type that will be created at some point. 
+      editButton.addEventListener("click", edit);
+      function edit(e) {
+        e.preventDefault();
+        let table = document.getElementsByClassName("row")[0];
+
+        make.value = table.children[0].innerText;
+        model.value = table.children[1].innerText;
+        productionYear.value = table.children[2].innerText;
+        fuelType.value = table.children[3].innerText;
+        originalCost.value = table.children[4].innerText;
+        sellingPrice.value = table.children[5].innerText;
+
+        e.target.parentElement.parentElement.remove()
+
+      }
+      sellButton.addEventListener("click", sell);
+      function sell(e) {
+
+
+        let ulCarList = document.getElementById("cars-list");
+        let li = document.createElement("li");
+        li.classList.add("each-list");
+
+        let span1 = document.createElement("span");
+        let currTable = e.target.parentElement.parentElement;
+        span1.innerText = `${currTable.children[0].textContent} ${currTable.children[1].textContent}`
+
+        let span2 = document.createElement("span");
+        span2.innerText = `${currTable.children[2].textContent}`;
+
+        let span3 = document.createElement("span");
+        let diff = Number(currTable.children[5].textContent) - Number(currTable.children[4].textContent)
+        span3.innerText = `${diff}`
+
+        li.appendChild(span1);
+        li.appendChild(span2);
+        li.appendChild(span3);
+        ulCarList.appendChild(li);
+// Calculate the profit at the bottom fo the page.
+        let profit = document.getElementById("profit");
+        let sum = Number(profit.textContent) + Number(span3.textContent);
+        profit.textContent = sum.toFixed(2);
+
+        e.target.parentElement.parentElement.remove()
+      }
     }
 
-    document.getElementsByClassName("action-btn edit")[0].addEventListener("click", edit);
-
-    function edit(e) {
-      e.preventDefault();
-      let table = document.getElementsByClassName("row")[0];
-
-      make.value = table.children[0].innerText;
-      model.value = table.children[1].innerText;
-      productionYear.value = table.children[2].innerText;
-      fuelType.value = table.children[3].innerText;
-      originalCost.value = table.children[4].innerText;
-      sellingPrice.value = table.children[5].innerText;
-
-      e.target.parentElement.parentElement.remove()
-
-    }
-    document.getElementsByClassName("action-btn sell")[0].addEventListener("click",sell);
-    document.getElementsByClassName("action-btn sell")[1].addEventListener("click",sell);
-
-    
   }
-  function sell(e) {
-      
 
-    let ulCarList = document.getElementById("cars-list");
-    let li = document.createElement("li");
-    li.classList.add("each-list");
-    let span1 = document.createElement("span");
-    let currTable = e.target.parentElement.parentElement;
-    span1.innerText = `${currTable.children[0].textContent} ${currTable.children[1].textContent}`
-    let span2 = document.createElement("span");
-    span2.innerText = `${currTable.children[2].textContent}`;
-    let span3 = document.createElement("span");
-    let diff = Number(currTable.children[5].textContent) - Number(currTable.children[4].textContent)
-    span3.innerText = `${diff}`
-    li.appendChild(span1);
-    li.appendChild(span2);
-    li.appendChild(span3);
-    ulCarList.appendChild(li);
-
-
-    e.target.parentElement.parentElement.remove()
-  }
 }
