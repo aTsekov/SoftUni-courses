7a22babfc8f34f211599aa4ee180ea5915bf9de5
aTsekov <antoni.tsekov@gmail.com>
2022-11-03 09:46:04 +0100

    100/100 Bus schedule  time table

--- JS Aplications/Asynchronous Programming/02.Bus-Schedule/app.js
+++ JS Aplications/Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,12 +1,45 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+    let departButton = document.getElementById('depart');
+    let arriveButton = document.getElementById('arrive');
+    let infoBoxElement = document.getElementsByClassName("info")[0];
+    let currentStop = "";
+    let nextStop = "depot"
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+    
+        async function depart() {
+            try{
+                let urlBusStopId = `http://localhost:3030/jsonstore/bus/schedule/${nextStop}`;
+                const response = await fetch(urlBusStopId); 
+                const data = await response.json();
+                 currentStop = data.name;
+                 nextStop = data.next;
+                infoBoxElement.innerHTML = `Next stop ` + currentStop;
+                departButton.disabled = true;
+                arriveButton.disabled = false;
+
+            }
+            catch(error){
+                infoBoxElement.innerHTML = "Error"
+            }
+           
+            
+        }
+    
+        function arrive() {
+            try{
+            infoBoxElement.innerHTML = `Arriving at ` + currentStop;
+            departButton.disabled = false;
+            arriveButton.disabled = true;
+            }
+            catch(error){
+                infoBoxElement.innerHTML = "Error"
+            }
+        }
+
+    
+    
+    
 
     return {
         depart,
