39416952862c0dbd6b2f2f799bc18bd715371c5f
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-01-19 16:27:22 +0100

    Update from P01 to P012.sql

--- MS SQL/CRUD/from P01 to P012.sql
+++ MS SQL/CRUD/from P01 to P012.sql
@@ -57,6 +57,109 @@ SELECT [FirstName],[LastName]
     FROM [Employees]
    WHERE [Salary]  >= 50000
 ORDER BY [Salary] DESC
+
+-- P13
+SELECT 
+TOP(5)   [FirstName],
+         [LastName]
+ FROM    [Employees]
+ORDER BY [Salary] DESC
+
+--P14
+SELECT [FirstName],
+	   [LastName]
+  FROM [Employees]
+ WHERE [DepartmentID] NOT IN (4)
+
+--P15
+
+  SELECT *
+    FROM [Employees]
+ORDER BY [Salary] DESC
+		,[FirstName]
+		,[LastName] DESC
+		,[MiddleName]
+
+		- Problem 17
+-- Views store temporaly the SELECT query, not the resultset!!!
+GO
+ 
+CREATE VIEW [V_EmployeeNameJobTitle3]
+         AS
+            (
+                SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName])
+                    AS [Full Name],
+                       [JobTitle]
+                  FROM [Employees] 
+            )
+ 
+GO
+ 
+SELECT * FROM [V_EmployeeNameJobTitle3]
+ 
+GO
+ 
+SELECT [FirstName] + ' ' + ISNULL([MiddleName], 'Replacement') + ' ' + [LastName]
+                    AS [Full Name],
+                       [JobTitle]
+                  FROM [Employees] 
+ 
+-- Problem 19
+SELECT
+TOP (10) *--, FORMAT([StartDate], 'dd/MM/yyyy')
+    FROM [Projects]
+ORDER BY [StartDate],
+         [Name]
+ 
+-- Problem 21
+--Helper Queries
+SELECT *
+  FROM [Employees]
+ 
+SELECT [DepartmentID]
+  FROM [Departments]
+ WHERE [Name] IN ('Engineering', 'Tool Design', 'Marketing', 'Information Services')
+ 
+--Main Query Solution
+UPDATE [Employees]
+   SET [Salary] += 0.12 * [Salary]
+ WHERE [DepartmentID] IN (1, 2, 4, 11)
+ 
+SELECT [Salary]
+  FROM [Employees]
+ 
+--You can use Reverse Query if the changed value is const
+RESTORE DATABASE [SoftUni] FROM DISK = 'BackUpPath.bak'
+ 
+-- Advanced Solution (Subqueries)
+UPDATE [Employees]
+   SET [Salary] += 0.12 * [Salary]
+ WHERE [DepartmentID] IN 
+                         (
+                            SELECT [DepartmentID]
+                              FROM [Departments]
+                             WHERE [Name] IN ('Engineering', 'Tool Design', 'Marketing', 'Information Services')
+                         )
+GO
+ 
+USE [Geography]
+GO
+ 
+ 
+-- Problem 24
+--CASE WHEN -> If [CurrencyCode] = 'EUR' then display 'Euro'
+--             else display 'Not Euro'
+  SELECT [CountryName],
+         [CountryCode],
+         CASE [CurrencyCode]
+              WHEN 'EUR' THEN 'Euro'
+              ELSE 'Not Euro'
+         END
+      AS [Currency]
+    FROM [Countries]
+ORDER BY [CountryName]
+
+
  
 
 
