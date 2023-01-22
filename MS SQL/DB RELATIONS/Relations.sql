e8b6f5f8a2a7a817fd21c3a74c3aa92ced757d37
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-01-22 10:44:21 +0100

    Update Relations.sql
    Create a DB by following a diagram

--- MS SQL/DB RELATIONS/Relations.sql
+++ MS SQL/DB RELATIONS/Relations.sql
@@ -175,6 +175,49 @@ CREATE TABLE [OrderItems]
 	PRIMARY KEY ([OrderID],[ItemID])
 )
 
+-- P06 University Datebase
+
+CREATE DATABASE [University Datebase]
+
+USE [University Datebase]
+
+
+CREATE TABLE [Subjects]
+(
+	[SubjectID] INT PRIMARY KEY IDENTITY (1,1),
+	[SubjectName] NVARCHAR (100) NOT NULL
+)
+CREATE TABLE [Majors]
+(
+	[MajorID] INT PRIMARY KEY IDENTITY (1,1),	
+	[Name] NVARCHAR (50) NOT NULL
+)
+CREATE TABLE [Students]
+(
+	[StudentID] INT PRIMARY KEY IDENTITY,
+	[StudentNumber] NVARCHAR (50) NOT NULL,
+	[StudentName] NVARCHAR (50) NOT NULL,
+	[MajorID] INT FOREIGN KEY REFERENCES [Majors]([MajorID]) NOT NULL
+)
+
+
+CREATE TABLE [Agenda]
+(
+	[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]),	
+	[SubjectID] INT FOREIGN KEY REFERENCES [Subjects]([SubjectID]),
+	PRIMARY KEY ([StudentID],[SubjectID])
+)
+
+CREATE TABLE [Payments]
+(
+	[PaymentID] INT PRIMARY KEY IDENTITY,
+	[PaymentDate] DATETIME2 NOT NULL,
+	[Payment Amount] DECIMAL(8,2) NOT NULL,
+	[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]) NOT NULL
+)
+
+
+
 
 
 
