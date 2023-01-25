3574319f9ba8b9c1f4d08e316ad2f99dfd0121e1
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-01-25 19:27:43 +0100

    Update BuildIn Funcs.sql
    From 1st to 7th task

--- MS SQL/BUILD IN FUNCTIONS/BuildIn Funcs.sql
+++ MS SQL/BUILD IN FUNCTIONS/BuildIn Funcs.sql
@@ -1,6 +1,55 @@
 USE [SoftUni]
 GO
 
+--P01
+
 SELECT [FirstName],[LastName]
   FROM [Employees]
  WHERE [FirstName] LIKE ('Sa%')
+
+ --P02
+ SELECT [FirstName],[LastName] 
+   FROM [Employees]
+  WHERE [LastName] LIKE ('%ei%')
+
+--P03
+
+ SELECT [FirstName] 
+   FROM [Employees]
+  WHERE [DepartmentID] IN (3,10)AND
+  YEAR([HireDate])BETWEEN 1995 AND 2005
+
+--P04
+
+ SELECT [FirstName],[LastName]
+   FROM [Employees]
+  WHERE [JobTitle] NOT LIKE '%engineer%'
+
+--P05
+
+ 
+  SELECT [Name]
+    FROM [Towns]
+   WHERE LEN([Name]) BETWEEN 5 AND 6
+ORDER BY [Name]
+
+--P06
+
+ SELECT *
+    FROM [Towns]
+   WHERE LEFT([Name],1) IN ('M','K','B','E')
+ORDER BY [Name]
+
+--P07
+
+ SELECT *
+    FROM [Towns]
+   WHERE LEFT([Name],1) NOT IN ('R','B','D')
+ORDER BY [Name]
+
+
+  
+
+
+
+   
