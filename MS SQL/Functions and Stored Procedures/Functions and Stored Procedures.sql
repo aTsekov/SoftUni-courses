58b5de85f83c57d13325eb0694e163ace9f9dce1
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-02-03 17:26:20 +0100

    Update Functions and Stored Procedures.sql
    2nd and 3rd problem

--- MS SQL/Functions and Stored Procedures/Functions and Stored Procedures.sql
+++ MS SQL/Functions and Stored Procedures/Functions and Stored Procedures.sql
@@ -16,3 +16,33 @@ CREATE OR ALTER PROC [usp_GetEmployeesSalaryAbove35000]
 EXEC [dbo].[usp_GetEmployeesSalaryAbove35000]
 
 --P02
+
+CREATE OR ALTER PROC [usp_GetEmployeesSalaryAboveNumber] @minSalary DECIMAL(18,4)
+		 AS 
+	  BEGIN
+				SELECT [FirstName],
+                           [LastName]
+                      FROM [Employees]
+                     WHERE [Salary] >= @minSalary
+	    END
+
+EXEC [dbo].[usp_GetEmployeesSalaryAboveNumber] 48100
+
+--P03
+CREATE PROC [usp_GetTownsStartingWith](@searchedString NVARCHAR(50))
+
+		AS
+
+		BEGIN
+
+			   DECLARE @stringCount int = LEN(@searchedString)
+
+				SELECT [Name] FROM Towns
+
+				 WHERE LEFT([Name],@stringCount) = @searchedString
+
+		END
+ 
+
+ EXEC  [usp_GetTownsStartingWith] 'b'
+ SELECT *FROM Towns
