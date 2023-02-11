2e0fb00d52c1c344340bd488bc0f8e2f2f33c546
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-02-11 17:04:29 +0100

    Update Exam preparation - AirportDB.sql

--- MS SQL/Exam preparation/Exam preparation - AirportDB.sql
+++ MS SQL/Exam preparation/Exam preparation - AirportDB.sql
@@ -63,3 +63,56 @@ CREATE TABLE FlightDestinations
 )
 
 --P02
+INSERT INTO Passengers(FullName, Email)
+
+SELECT CONCAT(FirstName,' ', LastName),
+	   CONCAT(FirstName,'', LastName, '@gmail.com')
+  FROM Pilots
+ WHERE Id BETWEEN 5 AND 15
+
+-- P03
+
+UPDATE Aircraft
+   SET Condition = 'A'
+ WHERE Condition IN ('C','B') AND FlightHours IS NULL OR FlightHours <= 100 AND [Year] > 2013
+
+--P04
+
+DELETE  
+ FROM Passengers
+ WHERE LEN(FullName) <= 10
+
+ --P05
+
+  SELECT Manufacturer,Model,FlightHours,Condition 
+    FROM Aircraft
+ORDER BY FlightHours DESC
+
+--P06
+
+   SELECT p.FirstName,p.LastName, a.Manufacturer, a.Model,a.FlightHours
+     FROM Pilots
+       AS p
+     JOIN PilotsAircraft
+	   AS pa
+	   ON p.Id = pa.PilotId
+	 JOIN  Aircraft
+	   AS a
+	   ON a.Id = pa.AircraftId
+    WHERE  FlightHours IS NOT NULL and FlightHours <= 304
+ ORDER BY FlightHours DESC,
+		  p.FirstName
+
+--P07
+
+   SELECT fl.Id AS DestinationId,fl.[Start],p.FullName,a.AirportName,fl.TicketPrice 
+     FROM FlightDestinations
+	   AS fl
+ JOIN Passengers
+       AS p
+	   ON fl.PassengerId = p.Id
+ JOIN Airports
+       AS a
+	   ON a.Id = fl.AirportId
+    WHERE DATEPART(Day,fl.[Start]) % 2 = 0
+ ORDER BY fl.TicketPrice DESC, a.AirportName
