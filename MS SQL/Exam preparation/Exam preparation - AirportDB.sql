b8cb91a3b68cb518b658f5dad4fe3584361ea59c
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-02-12 12:13:53 +0100

    All problems solved
    Retake EXAM 10 DEC 2021
     100/100

--- MS SQL/Exam preparation/Exam preparation - AirportDB.sql
+++ MS SQL/Exam preparation/Exam preparation - AirportDB.sql
@@ -116,3 +116,105 @@ ORDER BY FlightHours DESC
 	   ON a.Id = fl.AirportId
     WHERE DATEPART(Day,fl.[Start]) % 2 = 0
  ORDER BY fl.TicketPrice DESC, a.AirportName
+
+ --P08
+
+ SELECT a.Id AS AircraftId, A.Manufacturer, a. FlightHours, 
+  COUNT (f.Id) AS FlightDestinationsCount,
+    AVG (f.TicketPrice) AS AvgPrice
+   FROM Aircraft
+     AS a
+   JOIN FlightDestinations
+     AS f
+	 ON a.Id = f.AircraftId
+GROUP BY a.Id, a.Manufacturer, A.FlightHours
+HAVING COUNT (f.Id) >= 2
+ORDER BY 4 DESC, 1
+
+--P09
+
+     SELECT p.FullName,
+	  COUNT (a.Id) AS CountOfAircraft,
+	  SUM (fd.TicketPrice) AS TotalPayed
+	  FROM Passengers AS p
+	  JOIN FlightDestinations AS fd ON p.Id = Fd.PassengerId
+	  JOIN Aircraft AS a ON fd.AircraftId = a.Id
+	  WHERE SUBSTRING(p.FullName, 2, 1) = 'a'
+	  GROUP BY p.Id, p.FullName
+	  HAVING COUNT (a.Id) >1
+	  ORDER BY p.FullName
+
+--P10
+
+  SELECT a.AirportName,fd.Start AS DayTime, fd.TicketPrice, p.FullName, ar.Manufacturer,ar.Model
+    FROM FlightDestinations
+      AS fd
+    JOIN Airports
+      AS a
+      ON fd.AirportId = a.Id
+    JOIN Passengers
+      AS p
+   	  ON p.Id = fd.PassengerId
+    JOIN Aircraft 
+      AS ar
+   	  ON ar.Id = fd.AircraftId
+   WHERE DATEPART(HOUR, fd.[Start]) >= 6.00 AND DATEPART(HOUR, fd.[Start]) <= 20.00 AND fd.TicketPrice > 2500
+ORDER BY ar.Model 
+
+--P11
+GO
+CREATE OR ALTER FUNCTION udf_FlightDestinationsByEmail(@email VARCHAR (50))
+    RETURNS INT
+             AS
+          BEGIN
+		   DECLARE @num INT;
+		   SET @num = (
+						SELECT COUNT(*) 
+						  FROM Passengers
+	                        AS p
+	                      JOIN FlightDestinations
+	                        AS fl
+	                    	ON fl.PassengerId = p.Id
+	                     WHERE p.Email = @email
+	                  GROUP BY p.Email  
+	                  )
+					  IF @num IS NULL RETURN 0
+					  RETURN @num
+            END
+
+			
+ 
+GO
+
+SELECT dbo.udf_FlightDestinationsByEmail ('PierretteDunmuir@gmail.com')
+			SELECT dbo.udf_FlightDestinationsByEmail('Montacute@gmail.com')
+			SELECT dbo.udf_FlightDestinationsByEmail('MerisShale@gmail.com')
+    
+	--P12
+
+GO
+CREATE PROCEDURE usp_SearchByAirportName (@airportName VARCHAR(70))
+              AS
+           BEGIN
+                     SELECT AirportName, pa.FullName, 
+		CASE
+		WHEN fd.TicketPrice <= 400 THEN 'Low'
+		WHEN fd.TicketPrice > 400 AND fd.TicketPrice <= 1500 THEN 'Medium'
+		WHEN fd.TicketPrice > 1500 THEN 'High'
+		END
+		AS LevelOfTickerPrice,
+		ac.Manufacturer, 
+		ac.Condition, [at].TypeName
+		FROM Airports AS ap
+		JOIN FlightDestinations AS fd ON ap.Id = fd.AirportId
+		JOIN Passengers AS pa ON fd.PassengerId = pa.Id
+		JOIN Aircraft AS ac ON fd.AircraftId = ac.Id
+		JOIN AircraftTypes as [at] ON ac.TypeId = [at].Id
+		WHERE AirportName = @airportName
+		ORDER BY ac.Manufacturer, pa.FullName
+             END
+ 
+GO
+
+
+EXEC usp_SearchByAirportName 'Sir Seretse Khama International Airport'
