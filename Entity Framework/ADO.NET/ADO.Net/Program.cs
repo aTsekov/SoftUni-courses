865cceb01042a8eb376ebfc57e6cb8be5d18800f
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-02-14 20:51:10 +0100

    ADO.NET Basics

--- Entity Framework/ADO.NET/ADO.Net/Program.cs
+++ Entity Framework/ADO.NET/ADO.Net/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System.Data.SqlClient;
+
+
+
+SqlConnection connection = new SqlConnection(Config.ConnectionString); //Using the Config class which consists the connection string so I can avoid uploading it
+//to github since it cointains info how to get connected to the DB. the config class should be part of GitIgnore.
+//With the help of this connection string we can give it as param to the Sql connection class and we would connect to the DB.
+
+connection.Open(); // Opens the connection to the DB
+
+using (connection) //we use using because SqlConnection is IDisposible. Meaning we must close the connection once we are done using it. 
+{
+    string query = "SELECT * FROM Employees WHERE DepartmentID = 7";
+    SqlCommand command = new SqlCommand(query, connection); // We provide the query and the connection. 
+    SqlDataReader reader = await command.ExecuteReaderAsync(); // We provide the query and the connection. 
+
+    //int emplCount = (int) await command.ExecuteScalarAsync(); //Scalar means it will returns only one value. 
+
+    //Console.WriteLine($"There are {emplCount} empls. in our company");
+
+    using (reader)
+    {
+        int index = 0;
+        while(reader.Read())//While we have rows to read, the loop will beitterating
+        {
+            string? name = reader["FirstName"]?.ToString(); //The ? is used to check if the field is null/
+            string lastName = reader[2]?.ToString();
+            Console.WriteLine($"{index++}. {name} {lastName}");
+        }
+    }
+    
+}
