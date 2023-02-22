2152a763e81a69981ec7f0c999d193c381efcaa5
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-02-22 18:11:55 +0100

    EFCore scaffolding exersice

---
+++ Entity Framework/EF Core/EFCore SoftUni DB/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore_SoftUni_DB
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
