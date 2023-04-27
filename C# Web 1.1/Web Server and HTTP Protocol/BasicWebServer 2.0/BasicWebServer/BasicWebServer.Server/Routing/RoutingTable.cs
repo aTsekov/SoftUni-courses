ab5c173da82e1665f85277ef4f1a39e6111aa7b1
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-04-27 19:20:56 +0200

    Routing table

---
+++ C# Web 1.1/Web Server and HTTP Protocol/BasicWebServer 2.0/BasicWebServer/BasicWebServer.Server/Routing/RoutingTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
+
+namespace BasicWebServer.Server.Routing
+{
+    public class RoutingTable : IRoutingTable
+    {
+        private readonly Dictionary<Method, Dictionary<string, Response>> routes;
+        public IRoutingTable Map(string url, Method method, Response response)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRoutingTable MapGet(string url, Response response)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRoutingTable MapPost(string url, Response response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
