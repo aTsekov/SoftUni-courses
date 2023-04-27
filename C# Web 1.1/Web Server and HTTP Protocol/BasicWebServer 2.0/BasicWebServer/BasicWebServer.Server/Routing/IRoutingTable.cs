165603b204987bfd476f519660004755a638f0c8
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-04-27 20:14:06 +0200

    Adding the Response Match class

--- C# Web 1.1/Web Server and HTTP Protocol/BasicWebServer 2.0/BasicWebServer/BasicWebServer.Server/Routing/IRoutingTable.cs
+++ C# Web 1.1/Web Server and HTTP Protocol/BasicWebServer 2.0/BasicWebServer/BasicWebServer.Server/Routing/IRoutingTable.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
 
 namespace BasicWebServer.Server.Routing
 {
-    internal class IRoutingTable
+    public interface IRoutingTable
     {
+        IRoutingTable Map(string url, Method method, Response response);
+
+        IRoutingTable MapGet(string url, Response response);
+        IRoutingTable MapPost(string url, Response response);
     }
 }
