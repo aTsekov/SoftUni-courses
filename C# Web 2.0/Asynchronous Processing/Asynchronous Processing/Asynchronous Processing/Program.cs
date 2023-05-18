09bec15592ed917ded91fd8cd41c64694cbeb66a
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-05-18 14:42:23 +0200

    Thread and Task

---
+++ C# Web 2.0/Asynchronous Processing/Asynchronous Processing/Asynchronous Processing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+
+namespace Asynchronous_Processing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numA = int.Parse(Console.ReadLine());
+            var numB = int.Parse(Console.ReadLine());
+
+            Thread evens = new Thread(() => PrintEvenNumber(numA, numB));
+            evens.Start();// schedules the thread for execution
+            evens.Join();// waits for the thread to finish its work (blocks the calling thread)
+
+            Console.WriteLine("Thread finished work ");
+
+        }
+
+        static void PrintEvenNumber(int numA, int numB)
+        {
+            for (int i = numA; i <= numB; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ C# Web 2.0/Asynchronous Processing/Asynchronous Processing/SumEvens in Background/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SumEvens_in_Background
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            //We open a task which creates a thread. On this new thread the calculation is done separately while the main thread 
+            //is available and it's used by the console every time we type a command. Great example how the 2 threads are working simultaneously
+            var task = Task.Run(() =>
+            {
+                for (long i = 0; i < 1000000000; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                    }
+                }
+            });
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "exit")
+                {
+                    return;
+                }
+                else if (line == "show")
+                {
+                    Console.WriteLine(sum);
+                }
+            }
+        }
+    }
+}
