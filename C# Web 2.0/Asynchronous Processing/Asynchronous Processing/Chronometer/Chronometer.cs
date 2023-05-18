6bdb396fb0a42cc16f73de3df61c11dcf7df3114
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-05-18 15:49:33 +0200

    Simple chronometer app

---
+++ C# Web 2.0/Asynchronous Processing/Asynchronous Processing/Chronometer/Chronometer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private Stopwatch _stopwatch;
+        private List<string> _laps;
+
+        public Chronometer()
+        {
+            _stopwatch = new Stopwatch();
+            _laps = new List<string>();
+        }
+
+        public string GetTime => _stopwatch.Elapsed.ToString(@"mm\:ss\.ffff");
+        public List<string> Laps => _laps;
+        public void Start()
+        {
+            _stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            _stopwatch.Stop();
+        }
+
+        public string Lap()
+        {
+            string result = GetTime;
+            _laps.Add(result);
+            return result;
+        }
+
+        public void Reset()
+        {
+            _stopwatch.Reset();
+            _laps.Clear();
+        }
+    }
+}
